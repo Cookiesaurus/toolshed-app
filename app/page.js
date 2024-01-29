7308826728e277e5065910a85bf85f78952103db
andyewmc <aerskine.ptech@gmail.com>
2024-01-28 21:26:20 -0500

    Update root page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,56 @@
-
-//This is the first page that you see 
+import SlideShow from "@/components/Carousel/Slideshow";
 
 export default function Page() {
   return (
-    <></>
+    <>
+    <SlideShow/>
+    <div className="homepage">
+    <h2 className="headers">Browsing By Categories</h2>
+    <div className="categories">
+        <div className="cateogry"></div>
+        <div className="cateogry"></div>
+        <div className="cateogry"></div>
+        <div className="cateogry"></div>
+        <div className="cateogry"></div>
+        <div className="cateogry"></div>
+        <div className="cateogry"></div>
+        <div className="cateogry"></div>
+        <div className="cateogry"></div>
+        <div className="cateogry"></div>
+    </div>
+    <h2> Popular Tools</h2>
+    <div className="popular-container">
+        <div className="popular-container">
+            <div className="popular"></div>
+            <div className="popular"></div>
+            <div className="popular"></div>
+            <div className="popular"></div>
+            <div className="popular"></div>
+        </div>
+    </div>
+
+    <h2>Browsing By Brands</h2>
+    <div className="brands">
+            <div className="brand"></div>
+            <div className="brand"></div>
+            <div className="brand"></div>
+            <div className="brand"></div>
+            <div className="brand"></div>
+            <div className="brand"></div>
+            <div className="brand"></div>
+            <div className="brand"></div>
+            <div className="brand"></div>
+            <div className="brand"></div>
+            <div className="brand"></div>
+            <div className="brand"></div>
+            <div className="brand"></div>
+            <div className="brand"></div>
+            <div className="brand"></div>
+            <div className="brand"></div>
+            <div className="brand"></div>
+            <div className="brand"></div>
+    </div>
+
+</div></>
   )
 }
