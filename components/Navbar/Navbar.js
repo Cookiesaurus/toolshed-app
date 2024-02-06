b8520cf2e7eaa99b367372d5e833b138f81ef1f1
andyewmc <aerskine.ptech@gmail.com>
2024-02-06 12:37:34 -0500

    Update navbar again

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -2,7 +2,7 @@
 
 import './navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartShopping, faUser, faMagnifyingGlass, faBars, faCheckToSlot, faHeart, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faUser, faMagnifyingGlass, faFileInvoice} from '@fortawesome/free-solid-svg-icons';
 import CatOverlay from '../CatOverlay/CatOverlay';
 import Link from 'next/link';
 
@@ -28,11 +28,9 @@ const Navbar = () => {
         <div className='account-hover'>
             <p className='navbar-account'><Link href={'/Account/Profile'}><FontAwesomeIcon icon={faUser} size='lg'/></Link></p>
             <div className='dropdown-content'>
-                <div className='account'><p>Account</p><p style={{color: '#FF0101'}}>Login</p></div> {/* This is going to have to be conditional based on if the user is logged in or not and will have to be styled inline */}
-                <Link href={'/Account/Profile'}><FontAwesomeIcon icon={faUser} inverse style={{color: '#6C757D', paddingRight: '10px'}}/><p>Profile</p></Link>
-                <Link href={'/Account/Rentals'}><FontAwesomeIcon icon={faCheckToSlot} inverse style={{color: '#6C757D', paddingRight: '10px'}}/><p>My rental items</p></Link>
-                <Link href={'/Account/Favorites'}><FontAwesomeIcon icon={faHeart} inverse style={{color: '#6C757D', paddingRight: '10px'}}/><p>Favorites</p> </Link>
-                <Link href={'/Account/Address'}><FontAwesomeIcon icon={faLocationDot} inverse style={{color: '#6C757D', paddingRight: '10px'}}/><p>Address Book</p></Link>
+                <div className='account'><p><Link href={'/settings'}>Account</Link></p><p><Link style={{color: '#FF0101'}} href={'/Login'}>Login</Link></p></div> {/* This is going to have to be conditional based on if the user is logged in or not and will have to be styled inline */}
+                <Link href={'/Account/Profile'}><FontAwesomeIcon icon={faUser} inverse style={{color: '#6C757D', paddingRight: '10px'}}/><p>Account Settings</p></Link>
+                <Link href={'/Account/Rentals'}><FontAwesomeIcon icon={faFileInvoice} inverse style={{color: '#6C757D', paddingRight: '10px'}}/><p>My transactions</p></Link>
             </div>
         </div>
     </div>
