bdf144e04b5d616d4e645414568e14e39867db9c
Michael Pacholarz <pacholarzswims@gmail.com>
2024-02-06 12:19:20 -0500

    Add files via upload

--- app/Login/page.js
+++ app/Login/page.js
@@ -1,20 +1,20 @@
-import './login.css';
-import Image from 'next/image';
+import Image from "next/image";
 import logo from '../public/images/toolshed_logo.png';
-
-
+import './login.css';
 export default function Page() {
     return (<>
     <div className="content">
-        <div className="loginbox">
+        <div className="title">
             <Image src={logo} alt="toolshed logo" width={200} height={200}></Image>
             <h1 className='pagetitle'>Sign In</h1>
-            <div><input className="input" type="text" placeholder={" Email address"}/></div>
-            <div><input className="input" type="text" placeholder={" Password"}/></div>
-            <button className="button"  type="button">Sign In</button>
+        </div>
+        <form className="loginbox">
+            <div><input className="input" type="email" placeholder={" myemail@example.com"}/></div>
+            <div><input className="input" type="password" placeholder={" Password"}/></div>
+            <div><input className="button" type="submit" value={"Submit"}></input></div>
             <p className='basetext'>New to Toolshed? <a className="link" href="">Sign up</a></p>
             <p className='basetext'>Forgot password? <a className="link" href="">Click here</a></p>
-        </div>
+        </form>
     </div>
     </>)
 }
