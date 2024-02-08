a5c17aeee83ac6dba22a9af5715e4b69e7ba56d8
DunceSlav <pacholarzswims@gmail.com>
2024-02-08 10:23:11 -0500

    CSS updates + Footer content
    updates some CSS for the Login and Sign up pages, as well as adding meaningful content to the footer based off of the high fidelity designs

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,5 +1,7 @@
 'use client'
 
+import Image from "next/image";
+import logo from '../../app/public/images/toolshed_logo.png';
 import './footer.css';
 
 const Footer = () => {
@@ -7,46 +9,43 @@ const Footer = () => {
     <>
     <div className='footer'>
        <div className='column-one'>
-        <p className='footer-title'> Title</p>
-        <p className='footer-content'> Content</p>
-        <p className='footer-content'> Content</p>
-        <p className='footer-content'> Content</p>
-        <p className='footer-content'> Content</p>
+          <Image src={logo} alt="toolshed logo" width={100} height={100}></Image>
+          <p className='footer-content'> Subscribe to our newsletter</p>
+          <input type="email" placeholder={" Enter your email"} style={{height: '40px'}}/>
        </div>
        <div className='column-two'>
-        <p className='footer-title'> Title</p>
-        <p className='footer-content'> Content</p>
-        <p className='footer-content'> Content</p>
-        <p className='footer-content'> Content</p>
-        <p className='footer-content'> Content</p>
-        <p className='footer-content'> Content</p>
+          <p className='footer-title'> Contact</p>
+          <p className='footer-content'> The Tool Shed 1255 University Ave, C010 (Lower Level) Rochester, NY 14607</p>
+          <p className='footer-content'> Email: toolshed@seacrochester.org</p>
+          <p className='footer-content'> Phone: 585-271-8665</p>
        </div>
        <div className='column-three'>
-        <p className='footer-title'>Title</p>
-        <p className='footer-content'> Catalog </p>
-        <p className='footer-content'> Catalog</p>
-        <p className='footer-content'> Catalog</p>
-        <p className='footer-content'> Catalog</p>
-        <p className='footer-content'> Catalog</p>
-        <p className='footer-content'> Catalog</p>
+          <p className='footer-title'>Pickup Hours</p>
+          <p className='footer-content'> Monday: Closed </p>
+          <p className='footer-content'> Tuesday: 3:00 PM to 7:00PM</p>
+          <p className='footer-content'> Wednesday: 3:00 PM to 7:00PM</p>
+          <p className='footer-content'> Thursday: 3:00 PM to 7:00PM</p>
+          <p className='footer-content'> Friday: 3:00 PM to 7:00PM</p>
+          <p className='footer-content'> Saturday: 10:00 AM to 2:00PM</p>
+          <p className='footer-content'> Sunday: Closed</p>
        </div>
        <div className='column-four'>
-        <p className='footer-title'> Contact</p>
-        <p className='footer-content'> contact.@email.com </p>
-        <p className='footer-content'>222-222-2222  333-333-3333</p>
-        <p className='footer-content'>123 Address Dr, Rochester, NY, 14623</p>
-        <div className='footer-logo'>
-          <span className="dot"></span>
-          <span className="dot"></span>
-          <span className="dot"></span>
-        </div>
+          <p className='footer-title'> Account</p>
+          <p className='footer-content'> My Account</p>
+          <p className='footer-content'> Login/Register</p>
+          <p className='footer-content'> Cart</p>
+       </div>
+       <div className='column-five'>
+          <p className='footer-title'> Quick Links</p>
+          <p className='footer-content'> Privacy Policy</p>
+          <p className='footer-content'> Terms of Use</p>
+          <p className='footer-content'> FAQ</p>
        </div>
     </div>
-    <hr/>
     <div className='footer-end'>
-      <p className='footer-content' style={{marginTop: '15px'}}>2023  SEAC Tool Shed</p>
-      <p className='footer-content' style={{marginTop: '15px'}}>Powered by </p>
+      <p className='footer-content' style={{textAlign: "center"}}>&copy; 2024  SEAC Tool Shed</p>
     </div>
+    <hr/>
     </>
   )
 }
