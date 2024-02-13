64d8589e2eae3c3114efa21e2c017834e7c14d17
andyewmc <aerskine.ptech@gmail.com>
2024-02-13 18:11:50 -0500

    Add in tool categories so their next to the slideshow

--- app/page.js
+++ app/page.js
@@ -4,7 +4,53 @@ import Link from "next/link";
 export default function Page() {
   return (
     <>
-    <SlideShow/>
+    <div className="slide-container">
+      <div className="categories">
+        <div className="category-link">
+          <Link href={'/'}>Crafting</Link>
+          <Link href={'/'} className="cat-arrow"> &#10095;</Link>
+        </div>
+        <div className="category-link">
+          <Link href={'/'}>Drill Extension</Link>
+          <Link href={'/'} className="cat-arrow"> &#10095;</Link>
+        </div>
+        <div className="category-link">
+          <Link href={'/'}>Drywall Tools</Link>
+          <Link href={'/'} className="cat-arrow"> &#10095;</Link>
+        </div>
+        <div className="category-link">
+          <Link href={'/'}>Electrical</Link>
+          <Link href={'/'} className="cat-arrow"> &#10095;</Link>
+        </div>
+        <div className="category-link">
+          <Link href={'/'}>Flooring</Link>
+          <Link href={'/'} className="cat-arrow"> &#10095;</Link>
+        </div>
+        <div className="category-link">
+          <Link href={'/'}>Masonry</Link>
+          <Link href={'/'} className="cat-arrow"> &#10095;</Link>
+        </div>
+        <div className="category-link">
+          <Link href={'/'}>Painting</Link>
+          <Link href={'/'} className="cat-arrow"> &#10095;</Link>
+        </div>
+        <div className="category-link">
+          <Link href={'/'}>Plumbing</Link>
+          <Link href={'/'} className="cat-arrow"> &#10095;</Link>
+        </div>
+        <div className="category-link">
+          <Link href={'/'}>Roofing</Link>
+          <Link href={'/'} className="cat-arrow"> &#10095;</Link>
+        </div>
+        <div className="category-link">
+          <Link href={'/'}>Woodworking</Link>
+          <Link href={'/'} className="cat-arrow"> &#10095;</Link>
+        </div>
+      </div>
+        <div className="slide-show">
+          <SlideShow/>
+        </div>
+    </div>
     <div className="homepage">
     <h2> Popular Tools</h2>
     <div className="popular-container">
@@ -17,7 +63,7 @@ export default function Page() {
         </div>
     </div>
     <div className="button-center">
-      <button type="submit"> <Link href={'/Inventory'} className="home-button">View popular tools</Link></button>
+      <button type="submit"> <Link href={'/Inventory'} className="home-button">View All Tools</Link></button>
     </div>
 
     <h2>Browsing By Brands</h2>
