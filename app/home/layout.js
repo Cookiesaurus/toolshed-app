a1b41995c35d1d365b4f6036dfef4a169e5f4627
andyewmc <aerskine.ptech@gmail.com>
2024-02-17 16:50:55 -0500

    Destructure home page into seperate components

--- app/home/layout.js
+++ app/home/layout.js
@@ -1,10 +1,8 @@
-import SlideShow from "@/components/Carousel/Slideshow";
 export default function Layout({
     children, // will be a page or nested layout
   }) {
     return (
     <>
-      <SlideShow/>
       {children}
     </>
     )
--- app/home/page.js
+++ app/home/page.js
@@ -2,54 +2,9 @@
 import './home.css';
 // `app/dashboard/page.js` is the UI for the `/dashboard` URL
 export default function Page() {
-    return (<>
-        <div className="homepage">
-        <h2 className="headers">Browsing By Categories</h2>
-        <div className="categories">
-            <div className="cateogry"></div>
-            <div className="cateogry"></div>
-            <div className="cateogry"></div>
-            <div className="cateogry"></div>
-            <div className="cateogry"></div>
-            <div className="cateogry"></div>
-            <div className="cateogry"></div>
-            <div className="cateogry"></div>
-            <div className="cateogry"></div>
-            <div className="cateogry"></div>
-        </div>
-        <h2> Popular Tools</h2>
-        <div className="popular-container">
-            <div className="popular-container">
-                <div className="popular"></div>
-                <div className="popular"></div>
-                <div className="popular"></div>
-                <div className="popular"></div>
-                <div className="popular"></div>
-            </div>
-        </div>
-
-        <h2>Browsing By Brands</h2>
-        <div className="brands">
-                <div className="brand"></div>
-                <div className="brand"></div>
-                <div className="brand"></div>
-                <div className="brand"></div>
-                <div className="brand"></div>
-                <div className="brand"></div>
-                <div className="brand"></div>
-                <div className="brand"></div>
-                <div className="brand"></div>
-                <div className="brand"></div>
-                <div className="brand"></div>
-                <div className="brand"></div>
-                <div className="brand"></div>
-                <div className="brand"></div>
-                <div className="brand"></div>
-                <div className="brand"></div>
-                <div className="brand"></div>
-                <div className="brand"></div>
-        </div>
-
-    </div>
-    </>)
+    return (
+    <>
+        
+    </>
+    )
   }
