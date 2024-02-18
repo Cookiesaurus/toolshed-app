d533efd95fe7ec07aed2af9e6ba85f80a4c1fbf0
andyewmc <aerskine.ptech@gmail.com>
2024-02-17 19:20:20 -0500

    Add in white class name to table and add tr as parent to td to supress errors

--- components/account/Transactions.js
+++ components/account/Transactions.js
@@ -5,20 +5,22 @@ const Transactions = () =>{
     <>
                     <h1>Transaction History</h1>
                     <div className='table-cont'>
-                        <table>
-                        <thead>
-                            <tr>
-                                <th>Type</th>
-                                <th>Date</th>
-                                <th>Payment Method</th>
-                                <th>Amount</th>
+                        <table className='white'>
+                        <thead className='white'>
+                            <tr className='white'>
+                                <th className='white'>Type</th>
+                                <th className='white'>Date</th>
+                                <th className='white'>Payment Method</th>
+                                <th className='white'>Amount</th>
                             </tr>
                         </thead>
-                        <tbody>
-                            <td>Gift Card</td>
-                            <td>17 May 2023</td>
-                            <td>Credit Card</td>
-                            <td>Amount</td>
+                        <tbody className='white'>
+                            <tr className='white'>
+                                <td className='white'>Gift Card</td>
+                                <td className='white'>17 May 2023</td>
+                                <td className='white'>Credit Card</td>
+                                <td className='white'>Amount</td>
+                            </tr>
                         </tbody>
                         </table>
                     </div>
