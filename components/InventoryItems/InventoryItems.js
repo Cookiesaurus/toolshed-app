0e96f8d0bb8ed0e4bd1b18c421e5a907b4948260
andyewmc <aerskine.ptech@gmail.com>
2024-02-19 11:43:44 -0500

    Create inventory items component

--- components/InventoryItems/InventoryItems.js
+++ components/InventoryItems/InventoryItems.js
@@ -1,9 +1,40 @@
+'use client'
 import React from 'react'
-
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 const InventoryItems = () => {
+    const [product, setProduct] = useState([]);
+    useEffect(() => {
+        fetch("https://dummyjson.com/products")
+        .then((res) => res.json())
+        .then((data) => {
+            setProduct(data.products);
+        });
+    }, []);
+
   return (
     <>
-      
+      <div className='inven-cont'>
+        <div className='inven-items'>
+            {product.slice(0, 24).map((productItem, index) => (
+                <Link key={index} href={'/product'}>
+                <div className='inventory-item'>
+                    <div className='inventory-image'>
+                        {<Image src={productItem.images[0]} alt={'Product Place Holder'} width={230} height={280} className="popular-img"/>}
+                    </div>
+                    <div className="product-info">
+                        <p>{productItem.title}</p>
+                        <div className="item-aval">
+                            <p className="stock-green">{productItem.stock} In Stock</p>
+                            <p className="light-paragraph">Main Location</p>
+                        </div>
+                    </div>
+                </div>
+                </Link>
+            ))}
+        </div>
+      </div>
     </>
   )
 }
