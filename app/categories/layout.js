31a0b4f258f82e8b76b8663b1e5b041210b6ef7e
andyewmc <aerskine.ptech@gmail.com>
2024-02-19 18:46:24 -0500

    Update imports

--- app/categories/layout.js
+++ app/categories/layout.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import './categories.css';
+import CatFilters from '@/components/CategoryFilters/CatFilters';
+import InventoryItems from '@/components/InventoryItems/InventoryItems';
 export default function Layout({
     children, // will be a page or nested layout
   }) {
@@ -16,16 +18,30 @@ export default function Layout({
     return (
       <>
       <div className='bread-crumb'>
-        <p><Link href={'/'} className='crumb-init'>SEAC Tool Shed /</Link></p><p className='crumb-extra'>Categories</p>
-      </div>
-      <div className="cat-page">
-        {categories}
-        
-      </div>
-      <h2>Most popular</h2>
-      <div className='popular-container'>
-        {popular}
-      </div>
+            <p className='crumb-init'><Link href={'/'}>SEAC Tool Shed /</Link></p>
+            <p className='crumb-init'><Link href={'/'}>Search /</Link></p>
+            <p className='crumb-extra'><Link href={'/'}>Specifc Search</Link></p>
+        </div>
+        <div className="conditions-cont">
+                <div className='conditions'>
+                    {/* <div className='inventory-cond'> Condition <span></span> </div>
+                    <div className='inventory-cond'> Condition <span></span> </div>
+                    <div className='inventory-cond'> Condition <span></span> </div> */}
+                </div>
+                <div className="sort">
+                    <select id="filter-sort">
+                        <option value="0">Sort By:</option>
+                        <option value="1">Featured</option>
+                        <option value="1">Popular</option>
+                        <option value="1">Price: Low to High</option>
+                        <option value="1">Price: High to Low</option>
+                    </select>
+                </div>
+            </div>
+        <div className='inventory-cont'>
+            <CatFilters/>
+            <InventoryItems/>
+        </div>
          {/* Have to add in the children param so that the components from the page appear within the layout */}
       </>
     )
