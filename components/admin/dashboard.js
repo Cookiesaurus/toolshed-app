aa507bc6ffbf786f90a2fd9b2bbdbba8475b6af4
DunceSlav <pacholarzswims@gmail.com>
2024-03-02 14:31:19 -0500

    Admin Page Creation
    Started making pages for the admin dashboard

---
+++ components/admin/dashboard.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import './dashboard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencil } from '@fortawesome/free-solid-svg-icons';
+const Profile = () =>{
+    return (
+        <>
+            <h1>Dashboard</h1>
+        </>
+    )
+}
+
+export default Profile
