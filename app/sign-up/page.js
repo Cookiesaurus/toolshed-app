cedd9a3f6f3c97bdf2e4c726c0f1e2c1c2a4952a
at1203-3010 <at1203@rit.edu>
2024-03-08 18:41:33 -0500

    Sign up page mostly working with validation

--- app/sign-up/page.js
+++ app/sign-up/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import logo from "../public/images/toolshed_logo.png";
 import "./signup.css";
 import { SignupForm } from "./sign-up-form";
+import Signupform from "@/components/FormComponents/signupform";
 // import { useState } from "react";
 
 export default async function Page() {
@@ -17,7 +18,8 @@ export default async function Page() {
                     ></Image>
                     <h1 className="pagetitle">Create an Account</h1>
                 </div>
-                <SignupForm></SignupForm>
+                {/* <SignupForm></SignupForm> */}
+                <Signupform></Signupform>
             </div>
         </>
     );
