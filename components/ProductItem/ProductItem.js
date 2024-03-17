2509204f1d7687d4252b2da12234c2e4beca5d42
andyewmc <aerskine.ptech@gmail.com>
2024-03-17 17:37:13 -0400

    Remove cart functionality from individual product

--- components/ProductItem/ProductItem.js
+++ components/ProductItem/ProductItem.js
@@ -2,28 +2,14 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import Image from 'next/image'
-import { formHandler } from '@/lib/actions/formHandler'
 const ProductItem = () => {
     const [product, setProduct] = useState([]);
-    const [quantity, setQuantity] = useState(0);
-    const handleMinusClick = () => {
-        if (quantity > 0) {
-          setQuantity(quantity - 1);
-        }
-      };
-    
-    const handlePlusClick = (maxStock) => {
-        if (quantity < maxStock) {
-          setQuantity(quantity + 1);
-        }
-      };
 
     useEffect(() => {
         fetch("https://dummyjson.com/products")
         .then((res) => res.json())
         .then((data) => {
             setProduct(data.products);
-            setQuantity(data.products[1].stock);
         });
     }, []);
 
@@ -48,24 +34,6 @@ const ProductItem = () => {
                         <p>Ready</p>
                         <p className='stock-green'>Today</p>
                     </div>
-                </div>
-                <div className='product-quantity'>
-                    <form className='quanity-form' action={formHandler} >
-                        <button type="submit" className='rent' id='add-to-cart'> Add to Cart </button>
-                        <div className='plus-minus'>
-                            <button onClick={handleMinusClick} className='minus'>-</button>
-                                <input 
-                                    id='quantity' 
-                                    type='number'
-                                    name='amount'
-                                    min='0' 
-                                    max={productItem.stock} 
-                                    value={quantity}
-                                    onChange={(e) => setQuantity(parseInt(e.target.value, 10) || 0)}
-                                />
-                            <button className='plus' onClick={() => handlePlusClick(productItem.stock)}>+</button>
-                        </div>
-                    </form>
                 </div>
                 <div className='replace-cont'>
                         <p className='replacement'>Replacement Cost:</p>
