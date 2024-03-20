61845568c6cc1a1430ad1c320632e713501523e2
at1203-3010 <at1203@rit.edu>
2024-03-19 20:15:40 -0400

    Made changes to the data migration script. Adding items and using home locations and current locations.

--- Database/datamigration.py
+++ Database/datamigration.py
@@ -37,14 +37,20 @@ def insertTools():
         Tool_Size = j['Size'] if not str(j['Size']) == "nan" else ""
 
         # Home_Location = int(j['Home Location']) if not str(j['Home Location']) == "nan" else 0
-        Home_Location = 1
+        Home_Location = j['Home Location']
+        location_q = "SELECT Tool_Location FROM Tool_Locations WHERE Location_Name='" + Home_Location + "'"
+        curr.execute(location_q)
+        Home_Location = curr.fetchone()[0]
 
         # Current_Location = j['Current Location'] if not str(j['Current Location']) == "nan" else 0
-        Current_Location = 1
+        Current_Location = j['Current Location']
+        location_q = "SELECT Tool_Location FROM Tool_Locations WHERE Location_Name='" + Current_Location + "'"
+        curr.execute(location_q)
+        Current_Location = curr.fetchone()[0]
 
         Location_Code = j['Location Code'] if not str(j['Location Code']) == "nan" else ""
         Tool_Description = j['Description'] if not str(j['Description']) == "nan" else ""
-        Tool_Status = int(j['Status']) if not str(j['Status']) == "nan" else 1
+        Tool_Status = int(j['Status(es)']) if not str(j['Status(es)']) == "nan" else 1
         Default_Loan_Length = j['Default Loan Length'] if not str(j['Default Loan Length']) == "nan" else 7
         Renewal_Amount = j['Maximum number of renewals'] if not str(j['Maximum number of renewals']) == "nan" else 1
         Default_Late_Fee = j['Daily Late Fee'] if not str(j['Daily Late Fee']) == "nan" else 1.0 #Required, available
@@ -68,22 +74,28 @@ def insertTools():
         Tool_Replacement_Cost = j['Replacement Cost'] if not str(j['Replacement Cost']) == "nan" else 0.0
         Tool_Replacement_Cost = float(Tool_Replacement_Cost)
 
-        #Adding the categories to the table 
-        Categories = j['Categories']
-        addCategories(Categories, Tool_ID)
-
-        #Adding the types to the table 
-        Types = j['Item Type']
-        addTypes(Types, Tool_ID)
-
         #Query using the tool IDs from excel
         query = """INSERT INTO Tools (Old_Tool_ID, Tool_Name, Brand_Name, Tool_Weight, Tool_Size, Home_Location, Current_Location, Location_Code, Tool_Description, Tool_Status, Tool_Image, Tool_Manual,Default_Late_Fee, Default_Loan_Length, Renewal_Amount, Tool_Replacement_Cost) 
         VALUES ('%s', '%s', '%s',  %f, '%s', %d, %d, '%s', '%s',  %d, "%s", "%s", %.2f, %d, %d, %.2f)""" % (Tool_ID, Tool_Name, Tool_Brand,Tool_Weight, Tool_Size, Home_Location, Current_Location, Location_Code, Tool_Description, Tool_Status, Image, Manual,Default_Late_Fee, Default_Loan_Length, Renewal_Amount,  Tool_Replacement_Cost)
         curr.execute(query)
+        conn.commit()
         # except:
         #     print(Tool_ID, Tool_Name, Tool_Brand,Tool_Weight, Tool_Size, Home_Location, Current_Location, Location_Code, Tool_Description, Tool_Status, Default_Late_Fee, Default_Loan_Length, Renewal_Amount,  Tool_Replacement_Cost)
         # Committing insertion
-        conn.commit()
+
+        new_query = "SELECT Tool_ID FROM Tools WHERE Old_Tool_ID='" + Tool_ID + "'"
+        curr.execute(new_query)
+        new_tool_id = curr.fetchone()[0]
+        print("New Tool ID : " + str(new_tool_id))
+
+        #Adding the categories to the table 
+        Categories = j['Categories']
+        addCategories(Categories, new_tool_id)
+
+        #Adding the types to the table 
+        Types = j['Item Type']
+        addTypes(Types, new_tool_id)
+
 
 
 
@@ -96,11 +108,9 @@ def addCategories( string , tool_id ):
     cat_indexes = list()
     for i in cat:
         query = "SELECT Category_ID FROM Categories WHERE Category_Name=\'" + str(i).strip() + "\'";
-        print(query)
         curr.execute(query)
         result = curr.fetchone()[0]
-        print(result)
-        insert_query = "INSERT INTO Tool_Categories Values (%d, %d)" % ( tool_id, result)
+        insert_query = "INSERT INTO Tool_Categories Values (%d, %d)" % ( tool_id, int(result))
 
         curr.execute(insert_query)
         conn.commit()
@@ -113,11 +123,9 @@ def addTypes( string, tool_id ):
     cat_indexes = list()
     for i in cat:
         query = "SELECT Type_ID FROM Types WHERE Type_Name=\'" + str(i).strip() + "\'";
-        print(query)
         curr.execute(query)
         result = curr.fetchone()[0]
-        print(result)
-        insert_query = "INSERT INTO Tool_Types Values (%d, %d)" % ( tool_id, result)
+        insert_query = "INSERT INTO Tool_Types Values (%d, %d)" % ( tool_id, int(result))
 
         curr.execute(insert_query)
         conn.commit()
