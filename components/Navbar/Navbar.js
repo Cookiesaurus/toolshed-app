9d779ec7db5d882ba6c5fa223b4a98f3e3bf082e
andyewmc <aerskine.ptech@gmail.com>
2024-03-21 11:12:30 -0400

    Add logic to update url params on change"

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -4,11 +4,28 @@ import './navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faMagnifyingGlass, faFileInvoice} from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
-import { formHandler } from '@/lib/actions/formHandler';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useCallback } from 'react';
 
 
 const Navbar = () => {
-  return (
+    const router = useRouter()
+    const pathname = usePathname()
+    const searchParams = useSearchParams()
+   
+    // Get a new searchParams string by merging the current
+    // searchParams with a provided key/value pair
+    const createQueryString = useCallback(
+      (name, value) => {
+        const params = new URLSearchParams(searchParams)
+        params.set(name, value)
+   
+        return params.toString()
+      },
+      [searchParams]
+    )
+
+    return (
     <nav className='navbar'>
         <Link href={'/'} aria-label='Go to home' className='navbar-link' >
             <p className='navbar-logo'>SEAC Tool Shed</p>
@@ -21,9 +38,11 @@ const Navbar = () => {
                 <p>View All Tools</p>
             </Link>
         </div>
-        <form className='navbar-search' action={formHandler}>
+        <form className='navbar-search'>
             <label htmlFor='navbar-search' className='sr-only'>Search products</label>
-            <input type="text" name="search" id='navbar-search' aria-label='search products'/>
+            <input type="text" name="search" id='navbar-search' aria-label='search products' onChange={(e)=>{
+                router.push(`/inventory/search` + `?` + createQueryString('search', e.target.value))
+            }}/>
             <button type="submit" className='navbar-searchIcon' aria-hidden={true} tabIndex={-1}>
                 <FontAwesomeIcon icon={faMagnifyingGlass} size='lg' style={{color: 'white', backgroundColor: 'black'}}/>
             </button>
