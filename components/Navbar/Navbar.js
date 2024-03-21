185accbee375e1a29514892357b327e51128fdea
andyewmc <aerskine.ptech@gmail.com>
2024-03-21 19:06:11 -0400

    Revert "Merge pull request #45 from Cookiesaurus/test-aryan"
    This reverts commit c453d4da8a0445fe491396f5011f9f23ef4a4a41, reversing
    changes made to b42715c6bba2a3b5c05bb16e9df492ef3dc05232.

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,149 +1,65 @@
-import "./navbar.css";
-import { logout } from "@/actions/actions";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faCartShopping,
-    faUser,
-    faMagnifyingGlass,
-    faFileInvoice,
-} from "@fortawesome/free-solid-svg-icons";
-import Link from "next/link";
-import { getSession } from "@/actions/actions";
+'use client'
+
+import './navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faMagnifyingGlass, faFileInvoice} from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useCallback } from 'react';
 
-const Navbar = async () => {
-    const session = await getSession();
+
+const Navbar = () => {
     const router = useRouter()
     const pathname = usePathname()
     const searchParams = useSearchParams()
-
+   
     // Get a new searchParams string by merging the current
     // searchParams with a provided key/value pair
     const createQueryString = useCallback(
-        (name, value) => {
-          const params = new URLSearchParams(searchParams)
-          params.set(name, value)
-     
-          return params.toString()
-        },
-        [searchParams]
-      )
+      (name, value) => {
+        const params = new URLSearchParams(searchParams)
+        params.set(name, value)
+   
+        return params.toString()
+      },
+      [searchParams]
+    )
 
-    console.log(session);
     return (
-        <div
-            className="navbar"
-            role="Navigation"
-            aria-label="Website Navigation"
-        >
-            <Link href={"/"} aria-label="Go to home" className="navbar-link">
-                <p className="navbar-logo">SEAC’s Tool Shed</p>
+    <nav className='navbar'>
+        <Link href={'/'} aria-label='Go to home' className='navbar-link' >
+            <p className='navbar-logo'>SEAC Tool Shed</p>
+        </Link>
+        <div className='giftcard-inventory'>
+            <Link href={'/giftcard'} aria-label='Go to gift cards' className='navbar-link'>
+                <p>Gift Card</p>
             </Link>
-            <div className="giftcard-inventory">
-                <Link
-                    href={"/giftcard"}
-                    aria-label="Go to gift cards"
-                    className="navbar-link"
-                >
-                    <p>Gift Card</p>
-                </Link>
-                <Link
-                    href={"/inventory"}
-                    aria-label="Go to inventory"
-                    className="navbar-link"
-                >
-                    <p>View All Tools</p>
-                </Link>
-            </div>
-            <form className="navbar-search" method="get">
-                <label htmlFor="navbar-search" className="sr-only">
-                    Search products
-                </label>
-                <input
-                    type="text"
-                    name="search"
-                    id="navbar-search"
-                    aria-label="search products"
-                    onChange={(e)=>{
-                        router.push(`/inventory/search` + `?` + createQueryString('search', e.target.value))
-                    }}
-                />
-                <button
-                    type="submit"
-                    className="navbar-searchIcon"
-                    aria-hidden={true}
-                    tabIndex={-1}
-                >
-                    <FontAwesomeIcon
-                        icon={faMagnifyingGlass}
-                        size="lg"
-                        style={{ color: "white", backgroundColor: "black" }}
-                    />
-                </button>
-            </form>
-            <p className="navbar-cart">
-                <Link href={"/cart"} className="navbar-link">
-                    <FontAwesomeIcon
-                        icon={faCartShopping}
-                        size="lg"
-                        aria-label="Go to cart"
-                        style={{ backgroundColor: "white" }}
-                    />
-                </Link>
-            </p>
-            <div className="account-hover">
-                <p className="navbar-account">
-                    <Link href={"/login"} className="navbar-link">
-                        {session.isLoggedIn ? "Profile" : "Login"}
-                    </Link>
-                </p>
-                {session.isLoggedIn && <div className="dropdown-content">
-                    <div className="account">
-                        <p className="navbar-link">
-                            <Link
-                                href={"/account/profile"}
-                                className="navbar-link"
-                            >
-                                {session.isLoggedIn
-                                    ? session.firstName
-                                    : "Profile Name "}
-                            </Link>
-                        </p>
-                            <form action={logout}>
-                                <button class="" type="submit">Logout</button>
-                            </form>
-                    </div>{" "}
-                    {/* This is going to have to be conditional based on if the user is logged in or not and will have to be styled inline */}
-                    <Link href={"/account/profile"}>
-                        <FontAwesomeIcon
-                            icon={faUser}
-                            style={{
-                                color: "black",
-                                paddingRight: "10px",
-                                backgroundColor: "white",
-                            }}
-                            aria-label="Go to  account settings"
-                        />
-                        <p>Account Settings</p>
-                    </Link>
-                    <Link href={"/account/profile"}>
-                        <FontAwesomeIcon
-                            icon={faFileInvoice}
-                            style={{
-                                color: "black",
-                                paddingRight: "10px",
-                                backgroundColor: "white",
-                            }}
-                            aria-label="Go to account transaction"
-                        />
-                        <p>My transactions</p>
-                    </Link>
-                </div>}
+            <Link href={'/inventory'} aria-label='Go to inventory' className='navbar-link'>
+                <p>View All Tools</p>
+            </Link>
+        </div>
+        <form className='navbar-search'>
+            <label htmlFor='navbar-search' className='sr-only'>Search products</label>
+            <input type="text" name="search" id='navbar-search' aria-label='search products' onChange={(e)=>{
+                router.push(`/inventory/search` + `?` + createQueryString('search', e.target.value))
+            }}/>
+            <button type="submit" className='navbar-searchIcon' aria-hidden={true} tabIndex={-1}>
+                <FontAwesomeIcon icon={faMagnifyingGlass} size='lg' style={{color: 'white', backgroundColor: 'black'}}/>
+            </button>
+        </form>
+        <div className='account-hover'>
+            <p className='navbar-account'><Link href={'/login'} className='navbar-link'>Login</Link></p>
+            <div className='dropdown-content'>
+                <div className='account'>
+                    <p className='navbar-link'><Link href={'/account/profile'} className='navbar-link'>Profile Name</Link></p>
+                    <p><Link style={{color: '#FF0101'}} href={'/login'} aria-label='Go to login' className='navbar-link'>Logout</Link></p>
+                    </div> {/* This is going to have to be conditional based on if the user is logged in or not and will have to be styled inline */}
+                <Link href={'/account/profile'}><FontAwesomeIcon icon={faUser} style={{color: 'black', paddingRight: '10px', backgroundColor: 'white'}} aria-label='Go to  account settings'/><p>Account Settings</p></Link>
+                <Link href={'/account/profile'}><FontAwesomeIcon icon={faFileInvoice} style={{color: 'black', paddingRight: '10px' , backgroundColor: 'white'}} aria-label='Go to account transaction' /><p>My transactions</p></Link>
             </div>
         </div>
-    );
-};
+    </nav>
+  )
+}
 
-
-export default Navbar;
+export default Navbar
