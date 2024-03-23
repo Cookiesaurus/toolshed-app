991409e5024cfeb1e1f4af0328543ac74beb0416
andyewmc <aerskine.ptech@gmail.com>
2024-03-23 14:33:41 -0400

    Add in functionality for updating URL query params on change of most input values

--- components/Filters/filters.js
+++ components/Filters/filters.js
@@ -1,22 +1,100 @@
 'use client'
 
-import Link from 'next/link';
 import './filters.css';
 import React from 'react';
-import { formHandler } from '@/lib/actions/formHandler'; 
+import { useSearchParams, useRouter } from 'next/navigation';
+import {useState } from 'react';
 
-const Filters = ({categories, brands, types}) => {
+const Filters = ({categories, brands, types, locations}) => {
+
+  //Arrays to hold the values of the input from the checkboxes
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [selectedBrands, setSelectedBrands] = useState([]);
+  const [selectedTypes, setSelectedTypes] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState([]);
+  const [selectedStock, setSelectedStock] = useState([]);
+
+  //Constructors to update URL Params 
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const filterParams = new URLSearchParams(searchParams)
+
+  //****ON CHANGE EVENT HANDLERS**** */
+  const handleCategoryChange = (event) => {
+    const categoryName = event.target.value;
+    const isChecked = event.target.checked;
+    setSelectedCategories((prevSelectedCategories) =>
+      isChecked
+        ? [...prevSelectedCategories, categoryName]
+        : prevSelectedCategories.filter((category) => category !== categoryName)
+    );
+
+    UpdateURLParams({ category: isChecked ? [...selectedCategories, categoryName] : selectedCategories.filter((category) => category !== categoryName) });
+  };
+
+  const handleBrandChange = (event) => {
+    const brandName = event.target.value;
+    const isChecked = event.target.checked;
+    setSelectedBrands((prevSelectedBrand) =>
+      isChecked
+        ? [...prevSelectedBrand, brandName]
+        : prevSelectedBrand.filter((brandName) => brandName !== brandName)
+    );
+
+    UpdateURLParams({ brand: isChecked ? [...selectedBrands, brandName] : selectedBrands.filter((brandName) => brandName !== brandName) });
+  };
+
+  const handleTypeChange = (event) => {
+    const toolType = event.target.value;
+    const isChecked = event.target.checked;
+    setSelectedTypes((prevSelectedType) =>
+      isChecked
+        ? [...prevSelectedType, toolType]
+        : prevSelectedType.filter((toolType) => toolType !== toolType)
+    );
+
+    UpdateURLParams({ type: isChecked ? [...selectedTypes, toolType] : selectedTypes.filter((toolType) => toolType !== toolType) });
+  };
+
+  const handleStockChange = (event) => {
+    const availability = event.target.value;
+    const isChecked = event.target.checked;
+    setSelectedStock((prevSelectedStock) =>
+      isChecked
+        ? [...prevSelectedStock, availability]
+        : prevSelectedStock.filter((availability) => availability !== availability)
+    );
+
+    UpdateURLParams({ in_stock: isChecked ? [...selectedStock, availability] : selectedStock.filter((availability) => availability !== availability) });
+  };
+
+  // Function to iterate through the arrays of input values and update the URL search qury params 
+  const UpdateURLParams = (params) =>{
+    for (const [key, value] of Object.entries(params)) {
+        filterParams.set(key, value)
+    }
+    router.push(`/inventory` + `?` + filterParams.toString())
+  }
+
+
     return (
         <>
             <div className='filters-cont'>
-              <form action={formHandler} className='inventory-filters white'>
+              <form className='inventory-filters white'>
                 <h4 className='filter-headers white'>Categories</h4> 
                 <div className='filter white'> 
                   {categories.map(category => (
                     <React.Fragment key={category.Category_ID}>
                       <label htmlFor={category.Category_Name} className='checkbox-container white'>
                       {category.Category_Name}
-                        <input type="checkbox" className='checkbox' id={category.Category_Name} name='category' value={category.Category_Name} />
+                        <input 
+                          type="checkbox" 
+                          className='checkbox' 
+                          id={category.Category_Name} 
+                          name='category' 
+                          value={category.Category_Name}
+                          onChange={(e)=>{handleCategoryChange(e)}} 
+                          />
                         <span className="checkmark"></span>
                       </label>
                     </React.Fragment>
@@ -29,7 +107,14 @@ const Filters = ({categories, brands, types}) => {
                       <React.Fragment key={brand.Brand_Name}>
                         <label htmlFor={brand.Brand_Name} className='checkbox-container white' >
                           {brand.Brand_Name}
-                          <input type="checkbox" className='checkbox' id={brand.Brand_Name}  name='brand' value={brand.Brand_Name}/>
+                          <input 
+                            type="checkbox" 
+                            className='checkbox' 
+                            id={brand.Brand_Name}  
+                            name='brand' 
+                            value={brand.Brand_Name}
+                            onChange={(e)=>{handleBrandChange(e)}} 
+                            />
                           <span className="checkmark"></span>
                         </label>
                       </React.Fragment>
@@ -43,7 +128,14 @@ const Filters = ({categories, brands, types}) => {
                       <React.Fragment key={type.Type_Name}>
                         <label htmlFor={type.Type_Name} className='checkbox-container white' >
                           {type.Type_Name}
-                          <input type="checkbox" className='checkbox' id={type.Type_Name}  name='brand' value={type.Type_Name}/>
+                          <input 
+                            type="checkbox" 
+                            className='checkbox' 
+                            id={type.Type_Name}  
+                            name='brand' 
+                            value={type.Type_Name}
+                             onChange={(e)=>{handleTypeChange(e)}} 
+                            />
                           <span className="checkmark"></span>
                         </label>
                       </React.Fragment>
@@ -53,7 +145,12 @@ const Filters = ({categories, brands, types}) => {
                 <div className='filter-toggle white'>
                     <p className='white'>In Stock Only</p>
                     <label className="switch white" htmlFor='availability'>
-                      <input type="checkbox" id='availability' name='in-stock'/>
+                      <input 
+                        type="checkbox" 
+                        id='availability' 
+                        name='in-stock'
+                        onChange={(e)=>{handleStockChange(e)}} 
+                        />
                       <span className="slider round"></span>
                     </label>
                 </div>
@@ -73,7 +170,6 @@ const Filters = ({categories, brands, types}) => {
                       </label>
                     </div>
                 </div>
-                <button type='submit' id='apply-filters'> Apply Filters</button>
                 </form>
             </div>
 
