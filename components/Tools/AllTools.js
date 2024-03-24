0b054c68859bde53f3132ec8211cc8d279e74c4d
andyewmc <aerskine.ptech@gmail.com>
2024-03-24 17:58:54 -0400

    Revert merge commit  237a1a

---
+++ components/Tools/AllTools.js
@@ -0,0 +1,7 @@
+// Decide whether it is going to be a server component or client
+const AllTools = () => {
+    // Get all tools from database
+    // Render all Tool Previews
+    // Maybe use props
+    return <></>;
+};
--- components/account/Profile.js
+++ components/account/Profile.js
@@ -1,91 +1,44 @@
-import Link from "next/link";
-import { useState, useEffect } from "react";
-import "./profile.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPencil } from "@fortawesome/free-solid-svg-icons";
-const Profile = () => {
-    const [session, setSession] = useState(null);
-    useEffect(() => {
-        fetch("http://localhost:3000/api/me")
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                setSession({
-                    user: data.user,
-                    isLoggedIn: data.isLoggedIn,
-                });
-            });
-    }, []);
-
+import Link from 'next/link';
+import './profile.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencil } from '@fortawesome/free-solid-svg-icons';
+const Profile = () =>{
     return (
         <>
-            <h1>Profile</h1>
-            <div className="account-name">
-                <div className="account-info">
-                    <p>Username</p>
-                    <Link href={""}>
-                        <FontAwesomeIcon
-                            icon={faPencil}
-                            style={{ backgroundColor: "white" }}
-                        />
-                    </Link>
-                </div>
-                <div className="account-email">
-                    <p className="light-paragraph">Email</p>
-                    {session && <p>{session.user.Email}</p>}
-                </div>
-            </div>
-            <div className="address-cont">
-                <div className="account-info">
-                    <p>Addresses</p>
-                    <Link href={""} className="new-address">
-                        {" "}
-                        + Add
-                    </Link>
-                </div>
-                <div>
-                    <div className="account-info">
-                        <p className="light-paragraph">Default Address</p>
-                        <Link href={"/"}>
-                            <FontAwesomeIcon
-                                icon={faPencil}
-                                style={{ backgroundColor: "white" }}
-                            />
-                        </Link>
-                    </div>
-                    <div className="address-info">
-                        {session &&
-                            session.user.First_Name &&
-                            session.user.Last_Name && (
-                                <p>
-                                    {session.user.First_Name +
-                                        " " +
-                                        session.user.Last_Name}
-                                </p>
-                            )}
-                        {session && session.user.Address_Line1 && (
-                            <p>{session.user.Address_Line1}</p>
-                        )}
-                        {session && session.user.Address_Line2 && (
-                            <p>{session.user.Address_Line2}</p>
-                        )}
-                        {session && session.user.City && (
-                            <p>{session.user.City}</p>
-                        )}
-                        {session && session.user.State && (
-                            <p>{session.user.State}</p>
-                        )}
-                        {session && session.user.Postal_Code && (
-                            <p>{session.user.Postal_Code}</p>
-                        )}
-                        {session && <p>USA</p>}
-                        <p>Phone Number</p>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
-};
 
-export default Profile;
+                    <h1>Profile</h1>
+                    <div className='account-name'>
+                        <div className='account-info'>
+                            <p>Username</p>
+                            <Link href={''}><FontAwesomeIcon icon={faPencil} style={{backgroundColor: 'white'}} /></Link>
+                        </div>
+                        <div className='account-email'>
+                            <p className='light-paragraph'>Email</p>
+                            <p>testemail@email.com</p> 
+                        </div>
+                    </div>
+                    <div className='address-cont'>
+                    <div className='account-info'>
+                        <p>Addresses</p>
+                        <Link href={''} className='new-address'> + Add</Link>
+                    </div>
+                    <div>
+                        <div className='account-info'>
+                            <p className='light-paragraph'>Default Address</p>
+                            <Link href={'/'}><FontAwesomeIcon icon={faPencil} style={{backgroundColor: 'white'}}/></Link>
+                        </div>
+                        <div className='address-info'>
+                            <p>Name</p>
+                            <p>Address Line One</p>
+                            <p>Address Town/City</p>
+                            <p>Address State, Zip Code</p>
+                            <p>Address Country</p>
+                            <p>Phone Number</p>
+                        </div>
+                    </div>
+                </div>
+        </>
+    )
+}
+
+export default Profile
