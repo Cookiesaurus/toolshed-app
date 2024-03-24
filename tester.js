aa7c823a2ee985207d6e7964bf22149521618307
Ian Dinga <iad2750@rit.edu>
2024-03-24 19:59:39 -0400

    Made changes to the tester for the filters section

--- tester.js
+++ tester.js
@@ -1,22 +1,45 @@
-var mysql = require('mysql');
+/*var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password",
   database: "SEAC_Tool_Shed"
-});
+});*/
 
-var url = new URL("https://toolshed.com/inventory/category?category_name=Crafting");
-var search = new URLSearchParams(url.search);
-var categoryName = search.get('category_name');
+const categories = ['Drill Extention', 'Roofing'];
+const brands = [];
+const types = ['Test'];
+const status = 1;
+const locations = ['Main Location'];
 
 
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT t.Tool_Name, c.Category_Name FROM Tools AS t INNER JOIN Tool_Categories as tc ON t.Tool_ID = tc.Tool_ID INNER JOIN Categories as c ON tc.Category_ID = c.Category_ID WHERE c.Category_Name = '" + categoryName + "'"
-  , function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
-});
+let whereClause = 'WHERE';
+if (categories.length > 0) {
+    whereClause += ` c.Category_Name IN (${categories.map(cat => `'${cat}'`).join(', ')}) AND`;
+}
+if (brands.length > 0) {
+    whereClause += ` t.Brand_Name IN (${brands.map(brand => `'${brand}'`).join(', ')}) AND`;
+}
+if (types.length > 0) {
+    whereClause += ` ty.Type_Name IN (${types.map(type => `'${type}'`).join(', ')}) AND`;
+}
+if (status < 1) {
+    whereClause += ` t.Tool_Status = ${status} AND`;
+}
+if (locations.length > 0) {
+    whereClause += ` tl.Location_Name IN (${locations.map(loc => `'${loc}'`).join(', ')}) AND`;
+}
+
+whereClause = whereClause.replace(/AND\s*$/, '');
+
+console.log(whereClause);
+
+//con.connect(function(err) {
+//  if (err) throw err;
+//  con.query("SELECT t.Tool_Name, c.Category_Name FROM Tools AS t INNER JOIN Tool_Categories as tc ON t.Tool_ID = tc.Tool_ID INNER JOIN Categories as c ON tc.Category_ID = c.Category_ID WHERE c.Category_Name = '" + categoryName + "'"
+//  , function (err, result, fields) {
+//    if (err) throw err;
+//    console.log(result);
+//  });
+//});
