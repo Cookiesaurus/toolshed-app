729cc420ff51ad3d510f9e95bff72844c8f8ce6a
DunceSlav <pacholarzswims@gmail.com>
2024-03-25 21:32:16 -0400

    Product Info page progress

--- app/inventory/product/page.js
+++ app/inventory/product/page.js
@@ -1,6 +1,10 @@
 "use client";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import ProductItem from '@/components/ProductItem/ProductItem';
+
+import './product.css';
+
 export default function Page() {
     const searchParams = useSearchParams();
     const productid = searchParams.get("product_id");
@@ -20,6 +24,8 @@ export default function Page() {
         <div>
             Tool ID: {tool && tool.Tool_ID} and Tool Name:{" "}
             {tool && tool.Tool_Name}{" "}
+
+            <ProductItem/>
         </div>
     );
 }
