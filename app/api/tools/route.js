2d29315064dceb5f1643e33964871f830dc1e0e0
at1203-3010 <at1203@rit.edu>
2024-03-26 14:13:54 -0400

    Fixed bug for stock slider, excluded disabled and maintenance from all inventory

--- app/api/tools/route.js
+++ app/api/tools/route.js
@@ -5,7 +5,7 @@ export const GET = async () => {
     const query =`SELECT Tools.Tool_ID, Tools.Tool_Name, Tool_Locations.Location_Name, Tool_Statuses.Tool_Status_Details, Tools.Tool_Link FROM Tools 
     INNER JOIN Tool_Locations ON Tools.Home_Location=Tool_Locations.Tool_Location 
     INNER JOIN Tool_Statuses ON Tools.Tool_Status=Tool_Statuses.Tool_Status 
-    INNER JOIN Tool_Categories ON Tools.Tool_ID=Tool_Categories.Tool_ID 
+    INNER JOIN Tool_Categories ON Tools.Tool_ID=Tool_Categories.Tool_ID WHERE Tool_Statuses.Tool_Status_Details IN ('Available', 'Checked Out')
     GROUP BY Tools.Tool_ID`;
     const allTools = await db.selectFromDB(query);
     // Debug
--- app/inventory/page.js
+++ app/inventory/page.js
@@ -5,30 +5,73 @@ import db from "../config/db.mjs";
 import { useSearchParams } from "next/navigation";
 import ToolCard from "@/components/ToolCard/ToolCard";
 
+//Helper function - not needed for now
+/*const checkForParams = (searchParams) => {
+    const cat = String(searchParams.category);
+    const typ = String(searchParams.type);
+    const bra = String(searchParams.brand);
+    const status = String(searchParams.in_stock);
+    const loc = String(searchParams.location);
+    const searchBar = String(searchParams.search);
+    // console.log("Cat : ", cat);
+    // console.log("Type : ", typ);
+    // console.log("Bra : ", bra);
+    // console.log("Status : ", status);
+    // console.log("Log : ", loc);
+    // console.log("Search bar : ", searchBar);
+    let params = [];
+    if (cat || typ || bra || status || loc || searchBar) {
+        if (cat && cat != "") params.push("category");
+        if (typ && typ != "") params.push("types");
+        if (bra && bra != "") params.push("brand");
+        if (status && status != "") params.push("status");
+        if (loc && loc != "") params.push("location");
+        if (searchBar && searchBar != "") params.push("searchBar");
+        console.log(params);
+        return true;
+    } else return false;
+}; */
 
 export default async function Page({ searchParams }) {
+    // let checkParam = checkForParams(searchParams);
     // This would be all inventory items
     const categories = await db.selectFromDB("SELECT * FROM Categories");
     const brands = await db.selectFromDB("SELECT * FROM Brands");
     const types = await db.selectFromDB("SELECT * FROM Types");
-    const locations = await db.selectFromDB("SELECT * FROM SEAC_Tool_Shed.Tool_Locations");
+    const locations = await db.selectFromDB(
+        "SELECT * FROM SEAC_Tool_Shed.Tool_Locations"
+    );
 
     const cat = String(searchParams.category).split(",");
     const typ = String(searchParams.type).split(",");
     const bra = String(searchParams.brand).split(",");
     let status = String(searchParams.in_stock);
-    if (status != 'on'){status = 'off';}
+    if (status != "on") {
+        status = "off";
+    }
     let useInven = true;
     let loc = String(searchParams.location).split(",");
-    let searchBar = String(searchParams.search).split(" "); 
+    let searchBar = String(searchParams.search).split(" ");
     let sqlQuery =
         "SELECT Tools.Tool_ID, Tools.Tool_Name, Tools.Brand_Name, Tool_Locations.Location_Name, Tool_Statuses.Tool_Status_Details, GROUP_CONCAT(DISTINCT Categories.Category_Name SEPARATOR ', ') AS Categories, GROUP_CONCAT(DISTINCT Types.Type_Name SEPARATOR ', ') AS Types, Tools.Tool_Link FROM Tools INNER JOIN Tool_Locations ON Tools.Home_Location = Tool_Locations.Tool_Location INNER JOIN Tool_Statuses ON Tools.Tool_Status=Tool_Statuses.Tool_Status INNER JOIN Tool_Categories ON Tools.Tool_ID=Tool_Categories.Tool_ID INNER JOIN Categories ON Tool_Categories.Category_ID=Categories.Category_ID INNER JOIN Tool_Types ON Tools.Tool_ID=Tool_Types.Tool_ID INNER JOIN Types ON Tool_Types.Type_ID=Types.Type_ID";
     let whereClause = " WHERE";
-    if (searchBar.length > 0 && searchBar[0] != "undefined" && searchBar[0] != "") {
+    if (
+        searchBar.length > 0 &&
+        searchBar[0] != "undefined" &&
+        searchBar[0] != ""
+    ) {
         whereClause += " (";
         for (let i = 0; i < searchBar.length; i++) {
             whereClause +=
-                " Tools.Tool_Name LIKE '%" + searchBar[i] + "%' OR Categories.Category_Name LIKE '%" + searchBar[i] + "%' OR Tools.Brand_Name LIKE '%" + searchBar[i] + "%' OR Types.Type_Name LIKE '%" + searchBar[i] + "%' OR";
+                " Tools.Tool_Name LIKE '%" +
+                searchBar[i] +
+                "%' OR Categories.Category_Name LIKE '%" +
+                searchBar[i] +
+                "%' OR Tools.Brand_Name LIKE '%" +
+                searchBar[i] +
+                "%' OR Types.Type_Name LIKE '%" +
+                searchBar[i] +
+                "%' OR";
         }
         whereClause = whereClause.replace(/OR\s*$/, "");
         whereClause += ") AND";
@@ -58,11 +101,11 @@ export default async function Page({ searchParams }) {
             .join(", ")}) AND`;
         useInven = false;
     }
-    if (status == 'off') {
+    if (status == "off") {
         whereClause += ` Tool_Statuses.Tool_Status_Details IN ('Available', 'Checked Out') AND`;
-    }
-    else if (status == 'on') {
+    } else if (status == "on") {
         whereClause += ` Tool_Statuses.Tool_Status_Details IN ('Available') AND`;
+        useInven = false;
     }
     whereClause = whereClause.replace(/AND\s*$/, "");
     if (whereClause != " WHERE") sqlQuery += whereClause;
