e4e9fe954c84250ee0fe775513e28ed8fd1ead0d
andyewmc <aerskine.ptech@gmail.com>
2024-03-28 19:38:20 -0400

    add navbar to layout and update page

--- app/account/page.js
+++ app/account/page.js
@@ -1,4 +1,17 @@
+import Link from 'next/link'
+import './profile.css'
+import Accountnav from '@/components/account/accountnav'
+export default function ProfilePage() { 
+    return (
+        <>
+        <div className='bread-crumb'>
+            <p className='crumb-init'><Link href={'/'}>Account/</Link></p>
+            <p className='crumb-extra'>Profile</p>
+        </div>   
 
-export default function Page() {
-    return <h1>Hello, Account Page!</h1>
-}
+    <div className="account-cont">
+        <Accountnav/>
+    </div>
+        </>
+    )
+ }
