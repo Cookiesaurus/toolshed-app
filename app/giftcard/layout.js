19fc6b28d9bfa927868bb9eb6e0c7bc4ed72f834
andyewmc <aerskine.ptech@gmail.com>
2024-03-28 19:34:09 -0400

    Add navbar component to each individual layout

--- app/giftcard/layout.js
+++ app/giftcard/layout.js
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import './giftcard.css';
-import Giftcards from "@/components/account/giftcards";
-import Accountnav from "@/components/account/accountnav";
+import Navbar from "@/components/Navbar/Navbar";
 export default function Layout({
     children, // will be a page or nested layout
   }) {
     return (
       <>
+    <Navbar/>
       {children}
       </>
     )
