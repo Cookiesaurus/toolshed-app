39bac16b2e782718d520baaab89dd1ce00cbdd36
andyewmc <aerskine.ptech@gmail.com>
2024-03-30 19:15:10 -0400

    Create the Checkout pages and apply to the page

--- app/admin/inventory/checkout/page.js
+++ app/admin/inventory/checkout/page.js
@@ -1,8 +1,17 @@
-export default function Page({searchParams}){
+import React from 'react';
+import CheckOut from '@/components/admin/CheckOut';
+import db from '@/app/config/db.mjs';
 
-    return(
-        <>
-            <h1>{searchParams.tool_id}</h1>
-        </>
-    )
+export default async function Page({ searchParams }) {
+  const tool_id = searchParams.tool_id;
+  const account_id = searchParams.account_id;
+  const account_name = searchParams.name;
+  //const customerData = await db.selectFromDB(`SELECT * FROM Accounts WHERE Accounts.First_Name = ${account_name}`);
+
+  return (
+    <>
+      <h1>Check Out</h1>
+      {<CheckOut />}
+    </>
+  );
 }
