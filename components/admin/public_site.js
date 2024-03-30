fb7d7f2826761410b042221a8dedc0f5a5ec2aa4
andyewmc <aerskine.ptech@gmail.com>
2024-03-30 13:43:27 -0400

    Renaming and deleting admin files

--- components/admin/public_site.js
+++
@@ -1,9 +0,0 @@
-const Public_Site = () =>{
-    return(
-        <>
-            <h1>This needs to redirect to the homepage of the main site</h1>
-        </>
-    )
-}
-
-export default Public_Site
