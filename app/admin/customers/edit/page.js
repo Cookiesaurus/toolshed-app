39f60f3a29c0611d2706781ffa1e347fc4b77c05
andyewmc <aerskine.ptech@gmail.com>
2024-03-30 19:10:20 -0400

    Add editing componenents to their perspective pages

--- app/admin/customers/edit/page.js
+++ app/admin/customers/edit/page.js
@@ -1,5 +1,11 @@
-export default function Page({searchParams}){
-    return(
-        <h1>{searchParams.account_id}</h1>
+import EditUser from "@/components/admin/EditUser"
+import db from "@/app/config/db.mjs"
+
+export default function Page() { 
+    //Need a query that pulls all customer info from the DB based on account ID
+    return (
+        <> 
+        <EditUser/>
+        </>
     )
-}
+ }
--- app/admin/inventory/edit/page.js
+++ app/admin/inventory/edit/page.js
@@ -1,7 +1,11 @@
+import db from "@/app/config/db.mjs"
+import EditItem from "@/components/admin/EditItem"
 export default function Page({searchParams}){
-
+    //make a select that gets the item info based on the id in the search param
+    //pass that data into the EditItem Componenet
     return(
-        <>
+        <>  
+            <EditItem/>
             <h1>{searchParams.tool_id}</h1>
         </>
     )
