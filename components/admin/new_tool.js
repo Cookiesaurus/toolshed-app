ee83569e45430a851bc1873b55ab79d057ed79dd
DunceSlav <pacholarzswims@gmail.com>
2024-04-02 23:04:24 -0400

    Changes to Create new user and create new items

--- components/admin/new_tool.js
+++ components/admin/new_tool.js
@@ -5,10 +5,8 @@ const New_Tool = () =>{
                 <h1 className="section-title-big">Add New Item</h1>
                 <form className="newItem">
                     <div className="section">
-                        <label className="form-label" for="itemID">Item ID</label>
-                        <input className="formInput" type="text" id="itemID" name="itemID" placeholder="1234567890"/>
                         <label className="form-label" for="itemName">Item Name</label>
-                        <input className="formInput" type="text" id="itemName" name="itemName" placeholder="Tooly McToolface"/>
+                        <input className="formInput" type="text" id="itemName" name="itemName"/>
                         <div className="sub-section">
                             <p className="section-title-small">Select a tool status</p>
                             <input className="form-checkbox" type="checkbox" id="available" name="available" value="available"/>
@@ -20,104 +18,57 @@ const New_Tool = () =>{
                             <input className="form-checkbox" type="checkbox" id="disabled" name="disabled" value="disabled"/>
                             <label className="form-label" for="disabled">Disabled</label>
                         </div>
-                        <label className="form-label" for="manufacturer">Manufacturer</label>
-                        <input className="formInput" type="text" id="manufacturer" name="manufacturer" placeholder="manufacturer name"/>
-                        <label className="form-label" for="model">Model</label>
-                        <input className="formInput" type="text" id="model" name="model" placeholder="model name"/>
-                        <label className="form-label" for="productCode">Product Code/UPC</label>
-                        <input className="formInput" type="text" id="productCode" name="productCode" placeholder="1234567890"/>
-                        <label className="form-label" for="serialNum">Serial Number</label>
-                        <input className="formInput" type="text" id="serialNum" name="serialNum" placeholder="1234567890"/>
-                        <label className="form-label" for="size">Size</label>
-                        <input className="formInput" type="text" id="size" name="size" placeholder="input size"/>
-                        <label className="form-label" for="mainLoc">Main Location</label>
-                        <select className="formInput" name="mainLoc" id="mainLoc">
-                            <option value="main">Main Location</option>
-                            <option value="truck">Mobile Truck</option>
-                        </select>
-                        <label className="form-label" for="currentLoc">Current Location</label>
-                        <select className="formInput" name="currentLoc" id="currentLoc">
-                            <option value="main">Main Location</option>
-                            <option value="truck">Mobile Truck</option>
-                        </select>
-                    </div>
-                    <div className="section">
-                        <label className="form-label" for='image'>Upload an image</label>
-                        <input className="form-input" type="file" id="image" name="image"/>
-                        <label className="form-label" for='additionalFile'>Upload a file</label>
-                        <input className="form-input" type="file" id="additionalFile" name="additionalFile"/>
-                        <label className="form-label" for='description'>Description</label>
-                        <textarea id="description" name="description" rows="4" cols="50"/>
-                        <label className="form-label" for="locationCode">Location Code</label>
-                        <input className="formInput" type="text" id="locationCode" name="locationCode"/>
                         <div className="sub-section">
                             <p className="section-title-small">Categories</p>
-                            <input className="form-checkbox" type="checkbox" id="crafting" name="crafting" value="crafting"/>
-                            <label className="form-label" for="crafting">Crating</label>
-                            <br/>
-                            <input className="form-checkbox" type="checkbox" id="drillExtension" name="drillExtension" value="drill_extension"/>
-                            <label className="form-label" for="drillExtension">Drill Extension</label>
-                            <br/>
-                            <input className="form-checkbox" type="checkbox" id="drywallTools" name="drywallTools" value="drywall_tools"/>
-                            <label className="form-label" for="drywallTools">Drywall Tools</label>
-                            <br/>
-                            <input className="form-checkbox" type="checkbox" id="electrical" name="electrical" value="electrical"/>
-                            <label className="form-label" for="electrical">Electrical</label>
-                            <br/>
-                            <input className="form-checkbox" type="checkbox" id="flooring" name="flooring" value="flooring"/>
-                            <label className="form-label" for="flooring">Flooring</label>
-                            <br/>
-                            <input className="form-checkbox" type="checkbox" id="masonry" name="masonry" value="masonry"/>
-                            <label className="form-label" for="masonry">Masonry</label>
-                            <br/>
-                            <input className="form-checkbox" type="checkbox" id="misc" name="misc" value="misc"/>
-                            <label className="form-label" for="misc">Misc</label>
-                            <br/>
-                            <input className="form-checkbox" type="checkbox" id="painting" name="painting" value="painting"/>
-                            <label className="form-label" for="painting">Painting</label>
-                            <br/>
-                            <input className="form-checkbox" type="checkbox" id="plumbing" name="plumbing" value="plumbing"/>
-                            <label className="form-label" for="plumbing">Plumbing</label>
-                            <br/>
-                            <input className="form-checkbox" type="checkbox" id="roofing" name="roofing" value="roofing"/>
-                            <label className="form-label" for="roofing">Roofing</label>
-                            <br/>
-                            <input className="form-checkbox" type="checkbox" id="sawBlades" name="sawBlades" value="saw_blades"/>
-                            <label className="form-label" for="sawBlades">Saw Blades</label>
-                            <br/>
-                            <input className="form-checkbox" type="checkbox" id="welding" name="welding" value="welding"/>
-                            <label className="form-label" for="welding">Welding</label>
-                            <br/>
-                            <input className="form-checkbox" type="checkbox" id="woodworking" name="woodworking" value="woodworking"/>
-                            <label className="form-label" for="woodworking">Woodwoorking</label>
+                            <input className="form-checkbox" type="checkbox" id="option1" name="option1" value="option1"/>
+                            <label className="form-label" for="option1">Pull categories from database</label>
                         </div>
-                        <label className="form-label" for="keyWords">Key Words</label>
-                        <input className="formInput" type="text" id="keyWords" name="keyWords"/>
-                        <label className="form-label" for='adminNotes'>Admin Notes</label>
-                        <textarea id="adminNotes" name="adminNotes" rows="4" cols="50"/>
-                        <label className="form-label" for="condition">Condition</label>
-                        <select className="formInput" name="condition" id="condition">
-                            <option value="new">New</option>
-                            <option value="good">Good</option>
-                            <option value="fair">Fair</option>
-                            <option value="bad">Bad</option>
+                        <div className="sub-section">
+                            <p className="section-title-small">Tool type</p>
+                            <input className="form-checkbox" type="checkbox" id="option2" name="option2" value="option2"/>
+                            <label className="form-label" for="option2">Pull tool types from database</label>
+                        </div>
+                        <label className="form-label" for="brand-name">Brand Name</label>
+                        <select className="formInput" id="brand-name" name="brand-name">
+                            <option>Insert brands from database</option>
                         </select>
-                        <label className="form-label" for="signOff">Form needs to be signed off before checking out</label>
-                        <select className="formInput" name="signOff" id="signOff">
-                            <option value="yes">Yes</option>
-                            <option value="no">no</option>
-                        </select>
-                        <label className="form-label" for="weight">Weight</label>
+                        <label className="form-label" for="weight">Weight in lbs</label>
                         <input className="formInput" type="text" id="weight" name="weight"/>
-                        <label className="form-label" for="ecoRating">ecoRating</label>
-                        <input className="formInput" type="text" id="ecoRating" name="ecoRating"/>
-                        <label className="form-label" for="embodiedCarbon">Embodied Carbon</label>
-                        <input className="formInput" type="text" id="embodiedCarbon" name="embodiedCarbon"/>
-                        <label className="form-label" for="emmsionFactor">Emmision Factor</label>
-                        <input className="formInput" type="text" id="emmisionFactor" name="emmisionFactor"/>
+                        <label className="form-label" for="size">Size in inches</label>
+                        <input className="formInput" type="text" id="size" name="size"/>                        
+                    </div>
+                    <div className="section">
+                        <label className="form-label" for='image'>Upload tool picture</label>
+                        <input className="form-input" type="file" id="image" name="image"/>
+                        <label className="form-label" for='additionalFile'>Upload tool manual/other docs</label>
+                        <input className="form-input" type="file" id="additionalFile" name="additionalFile"/>
+                        <label className="form-label" for="loanFee">Additional Loan fee for tool in $</label>
+                        <input className="form-input" type="text" id="loanFee" name="loanFee" defaultValue={"0"}></input>
+                        <label className="form-label" for="lateFee">Late fee for tool per dayin $</label>
+                        <input className="form-input" type="text" id="lateFee" name="lateFee" defaultValue={"1"}></input>
+                        <label className="form-label" for="loanLength">Length of Loan</label>
+                        <input className="form-input" type="text" id="loanLength" name="loanLength" defaultValue={"7"}></input>
+                        <label className="form-label" for="loanRenew">Number of times that a customer can renew loan</label>
+                        <input className="form-input" type="text" id="loanRenew" name="loanRenew" defaultValue={"1"}></input>
+                        <label className="form-label" for="replaceCost">Replacement Cost</label>
+                        <input className="form-input" type="text" id="replaceCost" name="replaceCost" ></input>
+                        <label className="form-label" for="dropOffLoc">Can be dropped off at any location</label>
+                        <select className="formInput" name="dropOffLoc" id="dropOffLoc">
+                            <option value={"1"}>Yes</option>
+                            <option value={"0"}>No</option>
+                        </select>
+                        <label className="form-label" for="featured">Featured on the home page</label>
+                        <select className="formInput" name="featured" id="featured">
+                            <option value={"1"}>Yes</option>
+                            <option value={"0"}>No</option>
+                        </select>
+                        <label className="form-label" for="homeLoc">Home Location</label>
+                        <select className="formInput" name="homeLoc" id="homeLoc">
+                            <option value="main">Insert locations from database</option>
+                        </select>
                     </div>
                 </form>
-                <button type="submit">Add item</button>
+                <button className="createNewItemButton" type="submit">Add item</button>
             </div>
         </>
     )
