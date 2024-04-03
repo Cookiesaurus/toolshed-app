11d2c5d448b19c6c4dad94e6b3693f63cbc2cf4c
andyewmc <aerskine.ptech@gmail.com>
2024-04-03 12:23:29 -0400

    Update create new user and new items

--- components/admin/CreateNewUser.js
+++ components/admin/CreateNewUser.js
@@ -1,157 +1,127 @@
 const CreateNewUser = () => {
-    return (
-        <>
+  return(
+    <>
+    <form>
         <h1>Create a new user</h1>
-      <form>
         <div className="top_right_side_all_content">
-          <div className="accountInfo">
-            <h2>Account</h2>
-            <br />
-            <label htmlFor="password">Password</label>
-            <br />
-            <input type="password" name="password" required />
-            <br />
-            <label htmlFor="re-enter_password">Re-enter password</label>
-            <br />
-            <input type="password" name="re-enter_password" required />
-            <br />
-            <input
-              className="checkbox"
-              type="checkbox"
-              id="show_passwords"
-              name="show"
-              value="show"
-            />
-            <label htmlFor="show"> Show passwords</label>
-          </div>
-  
-          <div className="membershipInfo">
-            <h2>Membership</h2>
-              <label htmlFor="createDate">Member created (Date)</label>
-              <br />
-              <input type="date" id="creation_date" name="createDate" />
-              <br />
-              <label htmlFor="memberID">Membership ID</label>
-              <br />
-              <input id="membership_ID" name="memberID" />
-              <br />
-              <label htmlFor="memberType">Membership Type</label>
-              <br />
-              <input id="membership_type" name="memberType" />
-              <br />
-              <label htmlFor="expiration">Expiration</label>
-              <br />
-              <input type="date" id="expiration_date" name="expiration" />
-          </div>
+            <div className="accountInfo">
+                <h2>Account</h2>
+                <label for="username">Username</label>
+                <br/>
+                <input type="text" name="username" required/>
+                <br/>
+                <label for="password">Password</label>
+                <br/>
+                <input type="password" name="password" required/>
+                <br/>
+                <label for="re-enter_password">Re-enter password</label>
+                <br/>
+                <input type="password" name="re-enter_password" required/>
+                <br/>
+                <input className="checkbox" type="checkbox" id="show_passwords" name="show" value="show"/>
+                <label for="show"> Show passwords</label>
+            </div>
+            <div className="membershipInfo">
+                <h2>Membership</h2>
+                <label for="createDate">Member created (Date)</label>
+                <br/>
+                <input type="date" id="creation_date" name="createDate"/>
+                <br/>
+                <label for="memberID">Membership ID</label>
+                <br/>
+                <input id="membership_ID" name="memberID"/>
+                <br/>
+                <label for="memberType">Membership Type</label>
+                <br/>
+                <input id="membership_type" name="memberType"/>
+                <br/>
+                <label for="expiration">Expiration</label>
+                <br/>
+                <input type="date" id="expiration_date" name="expiration"/>
+            </div>
+            <div className="primaryInfo">
+                <h2>Primary Info</h2>
+                <label for="firstName">First Name</label>
+                <br/>
+                <input type="text" id="first_Name" name="firstName"/>
+                <br/>
+                <label for="lastName">Last Name</label>
+                <br/>
+                <input type="text" id="last_Name" name="lastName"/>
+                <br/>
+                <label for="email">Email</label>
+                <br/>
+                <input type="email" id="email" name="email"/>
+                <br/>
+                <label for="phoneNumber">Phone Number</label>
+                <br/>
+                <input type="tel" id="phone_number" name="phoneNumber"/>
+                <br/>
+            </div>
+            <div className="secondaryInfo">
+                <h2>Secondary Info</h2>
+                <label for="firstName">First Name</label>
+                <br/>
+                <input type="text" id="first_Name" name="firstName"/>
+                <br/>
+                <label for="lastName">Last Name</label>
+                <br/>
+                <input type="text" id="last_Name" name="lastName"/>
+                <br/>
+                <label for="email">Email</label>
+                <br/>
+                <input type="email" id="email" name="email"/>
+                <br/>
+                <label for="phoneNumber">Phone Number</label>
+                <br/>
+                <input type="tel" id="phone_number" name="phoneNumber"/>
+                <br/>
+            </div>
         </div>
         <div className="bottom_right_side_all_content">
-          <div className="primaryInfo">
-            <h2>Primary Info</h2>
-            <label htmlFor="firstName">First Name</label>
-            <br />
-            <input type="text" id="first_Name" name="firstName" />
-            <br />
-            <label htmlFor="lastName">Last Name</label>
-            <br />
-            <input type="text" id="last_Name" name="lastName" />
-            <br />
-            <label htmlFor="gender">Gender</label>
-            <br />
-            <input type="text" id="gender" name="gender" />
-            <br />
-            <label htmlFor="dob">Expiration</label>
-            <br />
-            <input type="date" id="date_of_birth" name="dob" />
-            <br />
-            <label htmlFor="email">Email</label>
-            <br />
-            <input type="email" id="email" name="email" />
-            <br />
-            <label htmlFor="title">Title</label>
-            <br />
-            <input type="text" id="title" name="title" />
-            <br />
-            <label htmlFor="organization">Organization</label>
-            <br />
-            <input type="text" id="organization" name="organization" />
-            <br />
-            <label htmlFor="streetAddress">Street Address</label>
-            <br />
-            <input type="text" id="street_address" name="streetAddress" />
-            <br />
-            <label htmlFor="Line2">Apt/Floor</label>
-            <br />
-            <input type="text" id="2nd_Line" name="Line2" />
-            <br />
-            <label htmlFor="city">City</label>
-            <br />
-            <input type="text" id="city" name="city" />
-            <br />
-            <label htmlFor="zipCode">ZIP code</label>
-            <br />
-            <input type="text" id="zip_code" name="zipCode" />
-            <br />
-            <label htmlFor="phoneNumber">Phone Number</label>
-            <br />
-            <input type="tel" id="phone_number" name="phoneNumber" />
-            <br />
-          </div>
-  
-          <div className="secondaryInfo">
-            <h2>Secondary Info</h2>
-            <label htmlFor="firstName">First Name</label>
-            <br />
-            <input type="text" id="first_Name" name="firstName" />
-            <br />
-            <label htmlFor="lastName">Last Name</label>
-            <br />
-            <input type="text" id="last_Name" name="lastName" />
-            <br />
-            <label htmlFor="gender">Gender</label>
-            <br />
-            <input type="text" id="gender" name="gender" />
-            <br />
-            <label htmlFor="dob">Expiration</label>
-            <br />
-            <input type="date" id="date_of_birth" name="dob" />
-            <br />
-            <label htmlFor="email">Email</label>
-            <br />
-            <input type="email" id="email" name="email" />
-            <br />
-            <label htmlFor="title">Title</label>
-            <br />
-            <input type="text" id="title" name="title" />
-            <br />
-            <label htmlFor="organization">Organization</label>
-            <br />
-            <input type="text" id="organization" name="organization" />
-            <br />
-            <label htmlFor="streetAddress">Street Address</label>
-            <br />
-            <input type="text" id="street_address" name="streetAddress" />
-            <br />
-            <label htmlFor="Line2">Apt/Floor</label>
-            <br />
-            <input type="text" id="2nd_Line" name="Line2" />
-            <br />
-            <label htmlFor="city">City</label>
-            <br />
-            <input type="text" id="city" name="city" />
-            <br />
-            <label htmlFor="zipCode">ZIP code</label>
-            <br />
-            <input type="text" id="zip_code" name="zipCode" />
-            <br />
-            <label htmlFor="phoneNumber">Phone Number</label>
-            <br />
-            <input type="tel" id="phone_number" name="phoneNumber" />
-            <br />
-          </div>
+            <div className="additionalInfo">
+                <h2>Additional Info</h2>
+                <label for="gender">Gender</label>
+                <br/>
+                <input type="text" id="gender" name="gender"/>
+                <br/>
+                <label for="dob">Expiration</label>
+                <br/>
+                <input type="date" id="date_of_birth" name="dob"/>
+                <br/>
+                <label for="title">Title</label>
+                <br/>
+                <input type="text" id="title" name="title"/>
+                <br/>
+                <label for="organization">Organization</label>
+                <br/>
+                <input type="text" id="organization" name="organization"/>
+                <br/>
+            </div>
+            <div className="address">
+                <h2>Address</h2>
+                <label for="streetAddress">Street Address</label>
+                <br/>
+                <input type="text" id="street_address" name="streetAddress"/>
+                <br/>
+                <label for="Line2">Apt/Floor</label>
+                <br/>
+                <input type="text" id="2nd_Line" name="Line2"/>
+                <br/>
+                <label for="city">City</label>
+                <br/>
+                <input type="text" id="city" name="city"/>
+                <br/>
+                <label for="zipCode">ZIP code</label>
+                <br/>
+                <input type="text" id="zip_code" name="zipCode"/>
+                <br/>
+            </div>
         </div>
-      </form>
-      </>
-    );
+    </form>
+    <button className="createNewUserButton" type="submit">Create Account</button>
+    </>
+)
   };
   
   export default CreateNewUser;
--- components/admin/NewItem.js
+++ components/admin/NewItem.js
@@ -7,138 +7,79 @@ const NewItem = () => {
         console.log(file)
     };
     
-  return (
-    <>
-      <h2>Add new item</h2>
-        <form>
-        <div className="new-item-container">
-
-            <div className="new-item-left white">
-            <h3 className="white">Item Information</h3>
-
-            <div className="item-info white">
-                <label htmlFor="item-id" className="white"> Item ID </label>
-                <input type="text" name="item-id" />
-
-                <label htmlFor="item-name" className="white"> Item Name </label>
-                <input type="text" name="item-name" />
+    return(
+        <>
+            <div className="right-section">
+                <h1 className="section-title-big">Add New Item</h1>
+                <form className="newItem">
+                    <div className="section">
+                        <label className="form-label" for="itemName">Item Name</label>
+                        <input className="formInput" type="text" id="itemName" name="itemName"/>
+                        <div className="sub-section">
+                            <p className="section-title-small">Select a tool status</p>
+                            <input className="form-checkbox" type="checkbox" id="available" name="available" value="available"/>
+                            <label className="form-label" for="available">Available</label>
+                            <br/>
+                            <input className="form-checkbox" type="checkbox" id="maintenance" name="maintenance" value="maintenance"/>
+                            <label className="form-label" for="maintenance">Maintenance</label>
+                            <br/>
+                            <input className="form-checkbox" type="checkbox" id="disabled" name="disabled" value="disabled"/>
+                            <label className="form-label" for="disabled">Disabled</label>
+                        </div>
+                        <div className="sub-section">
+                            <p className="section-title-small">Categories</p>
+                            <input className="form-checkbox" type="checkbox" id="option1" name="option1" value="option1"/>
+                            <label className="form-label" for="option1">Pull categories from database</label>
+                        </div>
+                        <div className="sub-section">
+                            <p className="section-title-small">Tool type</p>
+                            <input className="form-checkbox" type="checkbox" id="option2" name="option2" value="option2"/>
+                            <label className="form-label" for="option2">Pull tool types from database</label>
+                        </div>
+                        <label className="form-label" for="brand-name">Brand Name</label>
+                        <select className="formInput" id="brand-name" name="brand-name">
+                            <option>Insert brands from database</option>
+                        </select>
+                        <label className="form-label" for="weight">Weight in lbs</label>
+                        <input className="formInput" type="text" id="weight" name="weight"/>
+                        <label className="form-label" for="size">Size in inches</label>
+                        <input className="formInput" type="text" id="size" name="size"/>                        
+                    </div>
+                    <div className="section">
+                        <label className="form-label" for='image'>Upload tool picture</label>
+                        <input className="form-input" type="file" id="image" name="image"/>
+                        <label className="form-label" for='additionalFile'>Upload tool manual/other docs</label>
+                        <input className="form-input" type="file" id="additionalFile" name="additionalFile"/>
+                        <label className="form-label" for="loanFee">Additional Loan fee for tool in $</label>
+                        <input className="form-input" type="text" id="loanFee" name="loanFee" defaultValue={"0"}></input>
+                        <label className="form-label" for="lateFee">Late fee for tool per dayin $</label>
+                        <input className="form-input" type="text" id="lateFee" name="lateFee" defaultValue={"1"}></input>
+                        <label className="form-label" for="loanLength">Length of Loan</label>
+                        <input className="form-input" type="text" id="loanLength" name="loanLength" defaultValue={"7"}></input>
+                        <label className="form-label" for="loanRenew">Number of times that a customer can renew loan</label>
+                        <input className="form-input" type="text" id="loanRenew" name="loanRenew" defaultValue={"1"}></input>
+                        <label className="form-label" for="replaceCost">Replacement Cost</label>
+                        <input className="form-input" type="text" id="replaceCost" name="replaceCost" ></input>
+                        <label className="form-label" for="dropOffLoc">Can be dropped off at any location</label>
+                        <select className="formInput" name="dropOffLoc" id="dropOffLoc">
+                            <option value={"1"}>Yes</option>
+                            <option value={"0"}>No</option>
+                        </select>
+                        <label className="form-label" for="featured">Featured on the home page</label>
+                        <select className="formInput" name="featured" id="featured">
+                            <option value={"1"}>Yes</option>
+                            <option value={"0"}>No</option>
+                        </select>
+                        <label className="form-label" for="homeLoc">Home Location</label>
+                        <select className="formInput" name="homeLoc" id="homeLoc">
+                            <option value="main">Insert locations from database</option>
+                        </select>
+                    </div>
+                </form>
+                <button className="createNewItemButton" type="submit">Add item</button>
             </div>
-            <div className="item-info white">
-                <p className="white">Status</p>
-                <label
-                htmlFor="status-maintance"
-                className="checkbox-container white"
-                >
-                In Maintance
-                <input
-                    type="checkbox"
-                    className="checkbox"
-                    id="status-maintance"
-                    name="category"
-                />
-                <span className="checkmark"></span>
-                </label>
-                <label
-                htmlFor="status-disabled"
-                className="checkbox-container white"
-                >
-                Disabled
-                <input
-                    type="checkbox"
-                    className="checkbox"
-                    id="status-disabled"
-                    name="category"
-                />
-                <span className="checkmark"></span>
-                </label>
-            </div>
-            
-            <div className="item-info white">
-                <label htmlFor="item-brand" className="white"> Brand </label>
-                <input type="text" name="item-brand" />
-
-                <label htmlFor="item-type" className="white"> Type </label>
-                <input type="text" name="item-type" />
-                
-                <label htmlFor="item-weight" className="white"> Weight </label>
-                <input type="text" name="item-weight" />
-            </div>
-                <div className="item-info white">
-                    <label htmlFor="item-main-location" className="white">Home Location</label>
-                    <select name="item-main-location" id="item-location" >
-                        <option value="volvo">Home</option>
-                    </select>
-                    
-                    <label htmlFor="item-current-location" className="white">Home Location</label>
-                    <select name="item-location" id="item-location">
-                        <option value="volvo">Home</option>
-                    </select> 
-
-                    <label
-                htmlFor="status-floating"
-                className="checkbox-container white"
-                >
-                Floating
-                <input
-                    type="checkbox"
-                    className="checkbox"
-                    id="status-floating"
-                    name="category"
-                />
-                <span className="checkmark"></span>
-                </label>
-                <label
-                htmlFor="status-home"
-                className="checkbox-container white"
-                >
-                Goes Home
-                <input
-                    type="checkbox"
-                    className="checkbox"
-                    id="status-home"
-                    name="category"
-                />
-                <span className="checkmark"></span>
-                </label>
-                </div>
-            </div>
-
-            <div className="new-item-right">
-                <div className="item-info">
-                    <p>Additional Item Information</p>
-                    <label htmlFor="item-image">Item image</label>
-                    <input type="file" name="item-image" onChange={handleFileChange} />
-
-                    <label htmlFor="item-attachements">Additional Attachements</label>
-                    <input type="file" name="item-attachements" onChange={handleFileChange} />
-                </div>
-                <div className="item-info">
-                    <label htmlFor="Item Description">Item Description</label>
-                    <textarea id="story" name="story" rows="5" cols="33">
-                       
-                    </textarea>
-                </div>
-                <div className="item-info">
-                    <label htmlFor="item-category">Category</label>
-                    <label
-                htmlFor="status-floating"
-                className="checkbox-container"
-                >
-                Category - To Do: pull this from DB
-                <input
-                    type="checkbox"
-                    className="checkbox"
-                    id="status-floating"
-                    name="category"
-                />
-                <span className="checkmark"></span>
-                </label>
-                </div>
-            </div>
-            </div>
-        </form>
-    </>
-  );
+        </>
+    )
 };
 
 export default NewItem;
