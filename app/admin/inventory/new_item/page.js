5b96e8e365dc9c35d9f9ccae376cd6418f51e4bc
andyewmc <aerskine.ptech@gmail.com>
2024-04-04 14:55:25 -0400

    Pull data from db in new item component

--- app/admin/inventory/new_item/page.js
+++ app/admin/inventory/new_item/page.js
@@ -1,6 +1,11 @@
 import NewItem from "@/components/admin/NewItem";
-export default function Page(){
+import db from "@/app/config/db.mjs";
+export default async function Page(){
+    const categories = await db.selectFromDB("SELECT * FROM Categories");
+    const brands = await db.selectFromDB("SELECT * FROM Brands");
+    const types = await db.selectFromDB("SELECT * FROM Types");
+    const locations = await db.selectFromDB("SELECT * FROM SEAC_Tool_Shed.Tool_Locations");
     return (<>
-        <NewItem/>
+        <NewItem categories={categories} brands={brands} types={types} locations={locations}/>
     </>)
 }
