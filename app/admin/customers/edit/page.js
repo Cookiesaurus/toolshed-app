863264ef3933ffe484b24e7b9c442506e67e05e3
andyewmc <aerskine.ptech@gmail.com>
2024-04-04 12:23:45 -0400

    Add in dynamic import for components to sipress warnings on client side

--- app/admin/customers/edit/page.js
+++ app/admin/customers/edit/page.js
@@ -1,11 +1,23 @@
 import EditUser from "@/components/admin/EditUser"
 import db from "@/app/config/db.mjs"
 
-export default function Page() { 
+export default async function Page({searchParams}) { 
     //Need a query that pulls all customer info from the DB based on account ID
+    let customerInfo = await db.selectFromDB(`Select * From Accounts Where Account_ID = ${searchParams.account_id}`)
+    customerInfo = JSON.parse(JSON.stringify(customerInfo))
+
+    const objectData = customerInfo.reduce((acc, obj) => {
+        const newObj = {};
+        for (const key in obj) {
+          newObj[key] = obj[key];
+        }
+        acc = newObj;
+        return acc;
+      }, {});
+
     return (
         <> 
-        <EditUser/>
+        <EditUser customerInfo={objectData}/>
         </>
     )
  }
--- app/admin/inventory/page.js
+++ app/admin/inventory/page.js
@@ -1,5 +1,7 @@
-import AdminInventory from "@/components/admin/AdminInventory"
+import dynamic from "next/dynamic"
 import db from "@/app/config/db.mjs"
+const AdminInventory = dynamic(() => import("@/components/admin/AdminInventory"), { ssr: false })
+
 export default async function Page() { 
     let inventory = await db.selectFromDB(`SELECT Tools.Tool_ID, Tools.Tool_Name, Tool_Locations.Location_Name, Tool_Statuses.Tool_Status_Details, Tools.Tool_Link FROM Tools 
     INNER JOIN Tool_Locations ON Tools.Home_Location=Tool_Locations.Tool_Location 
