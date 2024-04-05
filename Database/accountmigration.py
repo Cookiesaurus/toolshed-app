08860021f376d7264dd6d60779255b6f9c39354f
Ian Dinga <iad2750@rit.edu>
2024-04-05 11:13:12 -0400

    Updates to Migration script

--- Database/accountmigration.py
+++ Database/accountmigration.py
@@ -69,8 +69,6 @@ def insertAccounts():
         
         #needed to set the Membership level based on table
         memb_level = j['Current Membership Type']
-        if(memb_level == 'Registration'):
-            memb_level = 'Expired'
         if('Level' in memb_level):
             memb_level = memb_level.replace(' Level', '')
         level_query = "SELECT Membership_Level FROM Membership_Levels WHERE Membership_Title = '" + memb_level + "'"
@@ -102,7 +100,9 @@ def insertAccounts():
             Membership_Exp_obj = datetime.strptime(check_exp, '%m/%d/%Y') 
             Membership_Expiration_Date = Membership_Exp_obj.strftime('%Y-%m-%d')
             Membership_Status = 1
-            
+            if(Membership_Exp_obj.date() < datetime.now().date()):
+                Membership_Status = 2
+                   
         Priviledge_Level = 1
         if(Email == 'mike@seacrochester.org'):
             Priviledge_Level = 5
@@ -115,8 +115,8 @@ def insertAccounts():
         query = """INSERT INTO Accounts (First_Name, Last_Name, DOB, Gender_Code, Organization_Name, Email, Phone_Number, Address_Line1, Address_Line2, City, State, Postal_Code, Account_Creation_Date, Account_Notes, Membership_Level, Membership_Status, Membership_Auto_Renewal, Membership_Creation_Date, Membership_Expiration_Date, Privilege_Level, Password, Secondary_First_Name, Secondary_Last_Name, Secondary_Email, Secondary_Phone_Number)
         VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%d", "%s", "%s", "%s", "%s","%s","%s","%s","%s")""" % (First_Name, Last_Name, DOB, Gender_Code, Organization_Name, Email, Phone_Number, Address_Line1, Address_Line2, City, State, Postal_Code, Account_Creation_Date, Account_Notes, Membership_Level, Membership_Status, Membership_Auto_Renewal, Membership_Creation_Date, Membership_Expiration_Date, Priviledge_Level, Password, Secondary_First_Name, Secondary_Last_Name, Secondary_Email, Secondary_Phone)
         
-        curr.execute(query)
-        conn.commit()
+        #curr.execute(query)
+        #conn.commit()
         
         print('Added Account: ' + First_Name + ' ' + Last_Name)
         
