570ab5ca85747c898e2c0e4db04445e0c140aa2a
andyewmc <aerskine.ptech@gmail.com>
2024-04-06 21:25:17 -0400

    update create user and edit user components

--- components/admin/CreateNewUser.js
+++ components/admin/CreateNewUser.js
@@ -1,132 +1,124 @@
-"use client"
+"use client";
 import { useState } from "react";
 const CreateNewUser = () => {
-    const [showPasswords, setShowPasswords] = useState(false);
+  const [showPasswords, setShowPasswords] = useState(false);
 
   const togglePasswordVisibility = () => {
-    setShowPasswords(prevState => !prevState);
+    setShowPasswords((prevState) => !prevState);
   };
 
-  return(
+  return (
     <>
-    <form>
+      <form>
         <h1>Create a new user</h1>
-        <div className="top_right_side_all_content">
+        <div className="new-user-cont">
+          <div className="new-user-left">
             <div className="accountInfo">
-                <h2>Account</h2>
-                <br/>
-                <label htmlFor="password">Password</label>
-                <input type={showPasswords ? "text" : "password"} name="password" id="password" required/>
-                <br/>
-                <label htmlFor="re-enter_password">Re-enter password</label>
-                <br/>
-                <input type={showPasswords ? "text" : "password"} name="re-enter password" id="re-enter_password" required/>
-                <br/>
-                <input className="checkbox" type="checkbox" id="show_passwords" name="show" value="show" onChange={togglePasswordVisibility}/>
-                <label htmlFor="show"> Show passwords</label>
-            </div>
-            <div className="membershipInfo">
-                <h2>Membership</h2>
-                <label htmlFor="createDate">Member created (Date)</label>
-                <br/>
-                <input type="date" id="creation_date" name="createDate"/>
-                <br/>
-                <label htmlFor="memberID">Membership ID</label>
-                <br/>
-                <input id="membership_ID" name="memberID"/>
-                <br/>
-                <label htmlFor="memberType">Membership Type</label>
-                <br/>
-                <input id="membership_type" name="memberType"/>
-                <br/>
-                <label htmlFor="expiration">Expiration</label>
-                <br/>
-                <input type="date" id="expiration_date" name="expiration"/>
-            </div>
-            <div className="primaryInfo">
+              <h2>Account</h2>
+
+              <label htmlFor="password">Password</label>
+              <input
+                type={showPasswords ? "text" : "password"}
+                name="password"
+                id="password"
+                required
+              />
+              <label htmlFor="re-enter_password">Re-enter password</label>
+              <input
+                type={showPasswords ? "text" : "password"}
+                name="re-enter password"
+                id="re-enter_password"
+                required
+              />
+              <input
+                className="checkbox"
+                type="checkbox"
+                id="show-passwords"
+                name="show-passwords"
+                value="show"
+                onChange={togglePasswordVisibility}
+              />
+              <label htmlFor="show"> Show passwords</label>
+              <div className="primaryInfo">
                 <h2>Primary Info</h2>
-                <label htmlFor="firstName">First Name</label>
-                <br/>
-                <input type="text" id="first_Name" name="firstName"/>
-                <br/>
-                <label htmlFor="lastName">Last Name</label>
-                <br/>
-                <input type="text" id="last_Name" name="lastName"/>
-                <br/>
+                <label htmlFor="first_Name">First Name</label>
+                <input type="text" id="first_Name" name="firstName" />
+
+                <label htmlFor="last_Name">Last Name</label>
+                <input type="text" id="last_Name" name="lastName" />
+
                 <label htmlFor="email">Email</label>
-                <br/>
-                <input type="email" id="email" name="email"/>
-                <br/>
+                <input type="email" id="email" name="email" />
+
                 <label htmlFor="phone_number">Phone Number</label>
-                <br/>
-                <input type="tel" id="phone_number" name="phone number"/>
-                <br/>
+                <input type="tel" id="phone_number" name="phone number" />
+
+                <label htmlFor="gender">Gender</label>
+                <input type="text" id="gender" name="gender" />
+
+                <label htmlFor="date_of_birth">Date of Bith</label>
+                <input type="date" id="date_of_birth" name="date-of-birth" />
+
+                <label htmlFor="title">Title</label>
+                <input type="text" id="title" name="title" />
+
+                <label htmlFor="organization">Organization</label>
+                <input type="text" id="organization" name="organization" />
+
+                <label htmlFor="street_address">Street Address</label>
+                <input type="text" id="street_address" name="street-address" />
+
+                <label htmlFor="2nd_Line">Apt/Floor</label>
+                <input type="text" id="2nd_Line" name="street-address-two" />
+
+                <label htmlFor="city">City</label>
+                <input type="text" id="city" name="city" />
+
+                <label htmlFor="zip_code">ZIP code</label>
+                <input type="text" id="zip_code" name="zipCode" />
+              </div>
+            </div>
+          </div>
+          <div className="new-user-right">
+            <div className="membershipInfo">
+              <h2>Membership</h2>
+
+              <label htmlFor="memberType">Membership Type</label>
+              <input id="membership_type" name="memberType" />
+
             </div>
             <div className="secondaryInfo">
-                <h2>Secondary Info</h2>
-                <label htmlFor="secondary_first_name">First Name</label>
-                <br/>
-                <input type="text" id="secondary_first_name" name="secondary-first-name"/>
-                <br/>
-                <label htmlFor="secondary_last_name">Last Name</label>
-                <br/>
-                <input type="text" id="secondary_last_name" name="secondary-last-name"/>
-                <br/>
-                <label htmlFor="secondary_email">Email</label>
-                <br/>
-                <input type="email" id="secondary_email" name="secondary-email"/>
-                <br/>
-                <label htmlFor="secondary_number">Phone Number</label>
-                <br/>
-                <input type="tel" id="secondary_number" name="secondary-number"/>
-                <br/>
+              <h2>Secondary Info</h2>
+              <label htmlFor="secondary_first_name">First Name</label>
+              <input
+                type="text"
+                id="secondary_first_name"
+                name="secondary-first-name"
+              />
+
+              <label htmlFor="secondary_last_name">Last Name</label>
+              <input
+                type="text"
+                id="secondary_last_name"
+                name="secondary-last-name"
+              />
+
+              <label htmlFor="secondary_email">Email</label>
+              <input type="email" id="secondary_email" name="secondary-email" />
+
+              <label htmlFor="secondary_number">Phone Number</label>
+              <input type="tel" id="secondary_number" name="secondary-number" />
             </div>
+          </div>
         </div>
-        <div className="bottom_right_side_all_content">
-            <div className="additionalInfo">
-                <h2>Additional Info</h2>
-                <label htmlFor="gender">Gender</label>
-                <br/>
-                <input type="text" id="gender" name="gender"/>
-                <br/>
-                <label htmlFor="date_of_birth">Date of Bith</label>
-                <br/>
-                <input type="date" id="date_of_birth" name="date-of-birth"/>
-                <br/>
-                <label htmlFor="title">Title</label>
-                <br/>
-                <input type="text" id="title" name="title"/>
-                <br/>
-                <label htmlFor="organization">Organization</label>
-                <br/>
-                <input type="text" id="organization" name="organization"/>
-                <br/>
-            </div>
-            <div className="address">
-                <h2>Address</h2>
-                <label htmlFor="street_address">Street Address</label>
-                <br/>
-                <input type="text" id="street_address" name="street-address"/>
-                <br/>
-                <label htmlFor="2nd_Line">Apt/Floor</label>
-                <br/>
-                <input type="text" id="2nd_Line" name="street-address-two"/>
-                <br/>
-                <label htmlFor="city">City</label>
-                <br/>
-                <input type="text" id="city" name="city"/>
-                <br/>
-                <label htmlFor="zip_code">ZIP code</label>
-                <br/>
-                <input type="text" id="zip_code" name="zipCode"/>
-                <br/>
-            </div>
+        <div className="create-button-cont">
+          <button className="createNewUserButton" type="submit">
+            Create Account
+          </button>
         </div>
-    </form>
-    <button className="createNewUserButton" type="submit">Create Account</button>
+      </form>
     </>
-)
-  };
-  
-  export default CreateNewUser;
-  
+  );
+};
+
+export default CreateNewUser;
