f68b57ae26d3a402c5717b010af3f00dc7bf345e
andyewmc <aerskine.ptech@gmail.com>
2024-04-11 08:35:09 -0400

    hydrate session prop on account

--- app/account/layout.js
+++ app/account/layout.js
@@ -1,12 +1,15 @@
 import Navbar from "@/components/Navbar/Navbar";
 import Breadcrumb from "@/components/BreadCrumb";
 import Accountnav from "@/components/account/accountnav";
-export default function Layout({
+import { getSession } from "@/actions/actions";
+export default async function Layout({
   children
 }) {
+  let session = await getSession()
+  session = JSON.parse(JSON.stringify(session))
   return (
     <>
-      <Navbar />
+      <Navbar session={session}/>
       <Breadcrumb
         homeElement={"Home"}
         containerClasses={"bread-crumb"}
