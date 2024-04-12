11b22f2baac56a3e83657ba0a29340c860061cf5
DunceSlav <pacholarzswims@gmail.com>
2024-04-12 00:36:11 -0400

    profile info changes

--- components/account/Profile.js
+++ components/account/Profile.js
@@ -5,37 +5,60 @@ const Profile = ({user}) =>{
     
     return (
         <>
-
-                    <h1>Profile</h1>
+            <h1>Profile</h1>
+            <Link className="edit-button" href={''}><FontAwesomeIcon icon={faPencil} style={{backgroundColor: 'white'}} />Edit Info</Link>
+            <form className='profile_info'>
+                <div className='primary_info'>
+                    <h3 className='info-title'>Primary Info</h3>
                     <div className='account-name'>
                         <div className='account-info'>
-                            <p>{user.user.First_Name} {user.user.Last_Name}</p>
-                            <Link href={''}><FontAwesomeIcon icon={faPencil} style={{backgroundColor: 'white'}} /></Link>
+                            <label className='profile-label' for="name">Name: </label>
+                            <input className='profile-input' id='name' name='name' value={`${user.user.First_Name} ${user.user.Last_Name}`} />
                         </div>
                         <div className='account-email'>
-                            <p className='light-paragraph'>Email</p>
-                            <p>{user.user.Email}</p> 
-                        </div>
-                    </div>
-                    <div className='address-cont'>
-                    <div className='account-info'>
-                        <p>Addresses</p>
-                        <Link href={''} className='new-address'> + Add</Link>
-                    </div>
-                    <div>
-                        <div className='account-info'>
-                            <p className='light-paragraph'>Default Address</p>
-                            <Link href={'/'}><FontAwesomeIcon icon={faPencil} style={{backgroundColor: 'white'}}/></Link>
-                        </div>
-                        <div className='address-info'>
-                            <p>{user.user.Address_Line1} {user.user.Address_Line2}</p>
-                            <p>{user.user.City}</p>
-                            <p>{user.user.Postal_Code}</p>
-                            <p>{user.user.State}</p>
-                            <p>{user.user.Phone_Number}</p>
+                            <label className='profile-label' for="email">Email: </label>
+                            <input className='profile-input' id='email' name='email' value={`${user.user.Email}`} />
+                            <label className='profile-label' for='phone'>Phone: </label>
+                            <input className='profile-input' id='phone' name='phone' value={`${user.user.Phone_Number}`} />
                         </div>
                     </div>
                 </div>
+                <div className='secondary_info'>
+                    <h3 className='info-title'>Secondary Info</h3>
+                    <div className='account-name'>
+                        <div className='account-info'>
+                            <label className='profile-label' for="name">Name: </label>
+                            <input className='profile-input' id='name' name='name' value={``} />
+                        </div>
+                        <div className='account-email'>
+                            <label className='profile-label' for="email">Email: </label>
+                            <input className='profile-input' id='email' name='email' value={``} />
+                            <label className='profile-label' for='phone'>Phone: </label>
+                            <input className='profile-input' id='phone' name='phone' value={``} />
+                        </div>
+                    </div>
+                </div>
+                <div className='address-cont'>
+                    <div>
+                        <div className='account-info'>
+                            <h3 className='info-title'>Default Address</h3>
+                            <label className='profile-label' for='address_line1'>Address Line 1: </label>
+                            <input className='profile-input' id='address_line1' name='address_line1' value={`${user.user.Address_Line1}`} />
+                            <label className='profile-label' for='address_line2'>Address Line 2: </label>
+                            <input className='profile-input' id='address_line2' name='address_line2' value={`${user.user.Address_Line2}`} />
+                            <label className='profile-label' for='city'>City: </label>
+                            <input className='profile-input' id='city' name='city' value={`${user.user.City}`} />
+                            <label className='profile-label' for='postal_code'>Postal code: </label>
+                            <input className='profile-input' id='postal_code' name='postal_code' value={`${user.user.Postal_Code}`} />
+                            <label className='profile-label' for='state'>State: </label>
+                            <input className='profile-input' id='state' name='state' value={`${user.user.State}`} />
+                        </div>
+                    </div>
+                </div>
+                <div className='save-changes'>
+                    <button className="save-changes-button" type='submit'>Save Changes</button>
+                </div>
+            </form>
         </>
     )
 }
