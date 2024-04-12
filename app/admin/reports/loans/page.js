c349309aa54be46149521eee66c295ac560197d4
andyewmc <aerskine.ptech@gmail.com>
2024-04-12 13:26:02 -0400

    dynamically import tables to prevent hydration errors

--- app/admin/reports/loans/page.js
+++ app/admin/reports/loans/page.js
@@ -1,4 +1,5 @@
-import LoanReport from "@/components/admin/LoanReport";
+import dynamic from "next/dynamic"
+const LoanReport = dynamic(() => import("@/components/admin/reports/LoanReport"), { ssr: false })
 import db from "@/app/config/db.mjs";
 export default async function Page(){
 
