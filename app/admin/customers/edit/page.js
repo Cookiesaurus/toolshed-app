c5235a99c997724078c1dcc49b614cdaceaa6d3f
andyewmc <aerskine.ptech@gmail.com>
2024-04-12 15:01:13 -0400

    add in gender memberships and privileges to the edit user and pass those to the edit user

--- app/admin/customers/edit/page.js
+++ app/admin/customers/edit/page.js
@@ -1,10 +1,15 @@
 import EditUser from "@/components/admin/customers/EditUser"
 import db from "@/app/config/db.mjs"
-
+import { getSession } from "@/actions/actions";
 export default async function Page({searchParams}) { 
     //Need a query that pulls all customer info from the DB based on account ID
 
     let customerInfo;
+    let gender = await db.selectFromDB(`Select Gender_Name from Genders`)
+    let memberships = await db.selectFromDB(`SELECT Membership_Levels.Membership_Title FROM Membership_Levels;`)
+    let privilegeLevels = await db.selectFromDB(`select Privilege_Levels.Privilege_Title from Privilege_Levels`)
+    const user = await getSession();
+    let admin = user.user?.Privilege_Level;
     //check to see if account_id is a number 
     if(!isNaN(searchParams.account_id)){
       customerInfo = await db.selectFromDB(`SELECT Accounts.Account_ID, Accounts.First_Name, Accounts.Last_Name, Accounts.DOB, Genders.Gender_Name, Accounts.Organization_Name, Accounts.Email, Accounts.Password, Accounts.Phone_Number, Accounts.Address_Line1, Accounts.Address_Line2, Accounts.City, Accounts.State, Accounts.Postal_Code, Accounts.Secondary_First_Name, Accounts.Secondary_Last_Name, Accounts.Secondary_Email, Accounts.Secondary_Phone_Number, Accounts.Account_Creation_Date, Accounts.Account_Notes, Membership_Levels.Membership_Title, Current_Membership_Status.Membership_Status_Description, Accounts.Membership_Auto_Renewal, Accounts.Membership_Creation_Date, Accounts.Membership_Expiration_Date, Privilege_Levels.Privilege_Title FROM Accounts
@@ -17,7 +22,9 @@ export default async function Page({searchParams}) {
       console.log(searchParams.search)
     }
     customerInfo = JSON.parse(JSON.stringify(customerInfo))
-
+    gender = JSON.parse(JSON.stringify(gender));
+    memberships = JSON.parse(JSON.stringify(memberships));
+    privilegeLevels = JSON.parse(JSON.stringify(privilegeLevels))
     const objectData = customerInfo.reduce((acc, obj) => {
         const newObj = {};
         for (const key in obj) {
@@ -29,7 +36,13 @@ export default async function Page({searchParams}) {
 
     return (
         <> 
-        <EditUser customerInfo={objectData}/>
+        <EditUser 
+          customerInfo={objectData} 
+          privilegeLevels={privilegeLevels} 
+          memberships={memberships} 
+          genders={gender}
+          admin={admin > 3 ? true : false}
+          />
         </>
     )
  }
