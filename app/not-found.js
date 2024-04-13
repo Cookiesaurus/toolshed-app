fb75926320d536d6da04775c2027d402d9552b5a
andyewmc <aerskine.ptech@gmail.com>
2024-04-13 13:32:53 -0400

    add navabr to not found page

--- app/not-found.js
+++ app/not-found.js
@@ -1,11 +1,14 @@
 import Link from 'next/link'
- 
+import Navbar from '@/components/Navbar/Navbar'
 export default function NotFound() {
   return (
+    <>
+    <Navbar/>
     <div>
       <h2>404: Page Not Found</h2>
       <p>They page you are looking for could not be found</p>
       <Link href="/">Return Home</Link>
     </div>
+    </>
   )
 }
