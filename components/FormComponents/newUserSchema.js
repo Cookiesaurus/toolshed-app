c2338da18ca9d2b8c6e7df0af8b190fa3ae8ef7e
andyewmc <aerskine.ptech@gmail.com>
2024-04-13 17:16:46 -0400

    add in validations for creating tools

--- components/FormComponents/newUserSchema.js
+++ components/FormComponents/newUserSchema.js
@@ -26,4 +26,30 @@ export const UserSchema = z
 export const giftCardEmails = z.object({
     toEmail : z.string().email().min(1),
     fromEmail : z.string().email().min(1)
+});
+
+export const updateUserProfileSchema = z.object({
+    firstName: z.string().min(1),
+    lastName: z.string().min(1),
+    email: z.string().email().min(1),
+    phone: z.string().min(10, {message : 'Invalid phone number '}).max(10),
+    addressFirst: z.string({ required_error: " Address cannot be empty" }).min(1),
+    addressSecond: z.string(),
+    city: z.string({ required_error: " City cannot be empty" }).min(1),
+    state: z.string().min(1),
+    zipCode: z.string({ required_error: " Zip code cannot be empty" }).min(5)
+});
+
+export const newToolSchema = z.object({
+    category: z.string().min(1),
+    type: z.string().min(1),
+})
+
+export const newToolDigitsSchema = z.object({
+    weight: z.number().min(0),
+    size: z.number().min(0),
+    loanFee: z.number().multipleOf(0.01).min(0),
+    loanLength: z.number().min(0),
+    renewal: z.number().min(0),
+    replacementCost: z.number().multipleOf(0.01).min(0)
 })
