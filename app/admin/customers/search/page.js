fc1240c4c4b212e23abb6197c20840cd45edf803
andyewmc <aerskine.ptech@gmail.com>
2024-04-14 13:15:06 -0400

    add queries to both search pages

--- app/admin/customers/search/page.js
+++ app/admin/customers/search/page.js
@@ -5,10 +5,20 @@ const Search = dynamic(() => import("@/components/admin/customers/Search"), {
 });
 export default async function Page({ searchParams }) {
   let name = searchParams.searchTerm;
+
+  let searchResults = await db.selectFromDB(`SELECT Accounts.Account_ID, Accounts.First_Name, Accounts.Last_Name, Accounts.Organization_Name, 
+                        Membership_Levels.Membership_Title, Accounts.Membership_Expiration_Date FROM Accounts
+                        INNER JOIN Membership_Levels ON Accounts.Membership_Level = Membership_Levels.Membership_Level
+                        WHERE 
+                        Accounts.First_Name REGEXP "${name}" OR Accounts.First_Name REGEXP "${name}"
+                        OR Accounts.Last_Name REGEXP "${name}" OR Accounts.Last_Name REGEXP "${name}";`)
+    
+searchResults = JSON.parse(JSON.stringify(searchResults))
+console.log(searchResults)
   return (
     <>
       <h1>Search Results: </h1>
-      <Search />
+      <Search customerData={searchResults} />
     </>
   );
 }
--- app/admin/inventory/search/page.js
+++ app/admin/inventory/search/page.js
@@ -3,12 +3,26 @@ import dynamic from "next/dynamic";
 const Search = dynamic(() => import("@/components/admin/tools/SearchTools"), {
   ssr: false
 });
+
 export default async function Page({ searchParams }) {
-  let tool = searchParams.searchTerm;
+  let tool = String(searchParams.searchTerm).trim().split(" ");
+  let toolSearchResults = await db.selectFromDB(`SELECT Tools.Tool_ID, Tools.Tool_Name, Home.Location_Name AS Home_Location, Current.Location_Name AS Current_Location, Tools.Brand_Name, GROUP_CONCAT(DISTINCT Types.Type_Name SEPARATOR ', ') AS Types, Tool_Statuses.Tool_Status_Details FROM Tools 
+  INNER JOIN Tool_Locations AS Home ON Tools.Home_Location = Home.Tool_Location
+  INNER JOIN Tool_Locations AS Current ON Tools.Current_Location = Current.Tool_Location
+  INNER JOIN Tool_Statuses ON Tools.Tool_Status=Tool_Statuses.Tool_Status
+  INNER JOIN Tool_Types ON Tools.Tool_ID=Tool_Types.Tool_ID
+  INNER JOIN Types ON Tool_Types.Type_ID=Types.Type_ID
+  GROUP BY Tools.Tool_ID
+  HAVING 
+      Tools.Tool_Name REGEXP "${tool}" AND Tools.Tool_Name REGEXP "${tool}" AND Tools.Tool_Name REGEXP "${tool}"`)
+
+
+      
+      toolSearchResults = JSON.parse(JSON.stringify(toolSearchResults))
   return (
     <>
       <h1>Search Results: </h1>
-      <Search />
+      
     </>
   );
 }
