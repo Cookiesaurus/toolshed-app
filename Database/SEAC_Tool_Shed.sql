c472cf983288601c2c51c0aaab1176491374a761
Bryce Hofstrom <neohofstrom@gmail.com>
2024-04-14 00:18:13 -0400

    Removed Unsigned and added NOT NULL

--- Database/SEAC_Tool_Shed.sql
+++ Database/SEAC_Tool_Shed.sql
@@ -472,7 +472,7 @@ CREATE TABLE Transactions (
 
 CREATE TABLE Transaction_Payments (
     Transaction_ID INT UNSIGNED,
-    Invoice_Number VARCHAR(255) UNSIGNED,
+    Invoice_Number VARCHAR(255) NOT NULL,
     Payment_ID VARCHAR(2000) UNSIGNED NOT NULL,
     CONSTRAINT PK_Transaction_Payments PRIMARY KEY (Transaction_ID),
     CONSTRAINT FK_Transaction_Payments_Transactions FOREIGN KEY (Transaction_ID) REFERENCES Transactions (Transaction_ID)
