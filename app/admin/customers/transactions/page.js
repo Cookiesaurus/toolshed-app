4c46ceec996cc1503cedc8ad2180f1eb2ca6705b
andyewmc <aerskine.ptech@gmail.com>
2024-04-16 04:19:26 -0400

    pass data to add transaction component

--- app/admin/customers/transactions/page.js
+++ app/admin/customers/transactions/page.js
@@ -1,12 +1,18 @@
 import { getSession } from "@/actions/actions";
 import AddTransaction from "@/components/admin/customers/AddTransaction";
+import db from "@/app/config/db.mjs";
 export default async function Page({searchParams}) { 
+    const accountID = searchParams.account_id
     let user = await getSession()
     let admin = user.user?.Privilege_Level
+    let costumer = await db.selectFromDB(`SELECT First_Name, Last_Name, Email, Account_ID FROM Accounts WHERE Account_ID = ${accountID}`);
+    let transactionTypes = await db.selectFromDB(`SELECT Transaction_Type , Transaction_Details FROM Transaction_Types`);
     
+    costumer = JSON.parse(JSON.stringify(costumer));
+    transactionTypes = JSON.parse(JSON.stringify(transactionTypes));
     return (
         <> 
-            <AddTransaction/>
+            <AddTransaction customerData={costumer[0]} admin={admin > 3 ? true : false} transactionTypes={transactionTypes}/>
         </>
     )
  }
